ee341be4ea764a680476455a7913c472cc828761
Dominik Inführ <dominik.infuehr@gmail.com>
2015-03-20 16:46:41 +0100

    use LeaveState instead of bool

--- src/parser/retck.rs
+++ src/parser/retck.rs
@@ -1,3 +1,5 @@
+use std::default::Default;
+
 use ast::Expr;
 use ast::Function;
 use ast::Statement;
@@ -8,6 +10,76 @@ use error::ErrorCode;
 use error::ParseError;
 
 use lexer::position::Position;
+
+#[derive(PartialEq,Eq,Copy)]
+enum Leave {
+    Fix, Maybe, Not
+}
+
+impl Leave {
+    fn seq(self: Leave, other: Leave) -> Leave {
+        assert!(self != Leave::Fix);
+
+        match other {
+            Leave::Fix => Leave::Fix,
+            Leave::Maybe => Leave::Maybe,
+            Leave::Not => self
+        }
+    }
+
+    fn merge(self: Leave, other: Leave) -> Leave {
+        if self == other {
+            self
+        } else if self == Leave::Fix || other == Leave::Fix {
+            Leave::Maybe
+        } else if self == Leave::Maybe || other == Leave::Maybe {
+            Leave::Maybe
+        } else {
+            Leave::Not
+        }
+    }
+}
+
+struct LeaveState {
+    pub method: Leave,
+    pub block: Leave,
+}
+
+impl LeaveState {
+    fn new() -> LeaveState {
+        LeaveState { method: Leave::Not, block: Leave::Not }
+    }
+
+    fn method() -> LeaveState {
+        LeaveState { method: Leave::Fix, block: Leave::Not }
+    }
+
+    fn block() -> LeaveState {
+        LeaveState { method: Leave::Not, block: Leave::Fix }
+    }
+
+    fn seq(self: &mut LeaveState, o: LeaveState) {
+        self.method = self.method.seq(o.method);
+        self.block = self.block.seq(o.block);
+    }
+
+    fn merge(self: LeaveState, o: LeaveState) -> LeaveState {
+        LeaveState {
+            method: self.method.merge(o.method),
+            block: self.block.merge(o.block)
+        }
+    }
+
+    fn is_fix(self: &LeaveState) -> bool {
+        self.method == Leave::Fix || self.block == Leave::Fix
+    }
+}
+
+impl Default for LeaveState {
+    fn default() -> LeaveState {
+        LeaveState::new()
+    }
+}
 
 pub struct ReturnCheck {
     errors: Vec<ParseError>,
@@ -22,28 +94,28 @@ impl ReturnCheck {
         self.errors
     }
 
-    fn check_block(self: &mut ReturnCheck, stmts: &mut Vec<Box<Statement>>) -> bool {
+    fn check_block(self: &mut ReturnCheck, stmts: &mut Vec<Box<Statement>>) -> LeaveState {
+        let mut leave = LeaveState::new();
+
         if stmts.len() == 0 {
-            return false;
+            return leave;
         }
 
-        let mut return_occured = false;
-
         for stmt in stmts {
-            if return_occured {
+            if leave.is_fix() {
                 self.errors.push(ParseError {
                     position: stmt.position,
                     code: ErrorCode::UnreachableCode,
                     message: format!("statement unreachable")
                 });
 
-                return true;
+                return leave;
             }
 
-            return_occured = self.visit_stmt(stmt);
+            leave.seq(self.visit_stmt(stmt));
         }
 
-        return_occured
+        leave
     }
 }
 
@@ -58,16 +130,16 @@ fn end_position(s: &Statement) -> Position {
 }
 
 impl Visitor for ReturnCheck {
-    type Returns = bool;
+    type Returns = LeaveState;
 
-    fn visit_fct(self: &mut ReturnCheck, fct: &mut Function) -> bool {
-        let return_occured = if fct.return_type.is_unit() {
-            true
+    fn visit_fct(self: &mut ReturnCheck, fct: &mut Function) -> LeaveState {
+        let leave = if fct.return_type.is_unit() {
+            LeaveState::method()
         } else {
             self.visit_stmt(&mut fct.block)
         };
 
-        if !return_occured {
+        if leave.method != Leave::Fix {
             self.errors.push(ParseError {
                 position: end_position(&fct.block),
                 code: ErrorCode::NoReturnValue,
@@ -75,19 +147,20 @@ impl Visitor for ReturnCheck {
             })
         }
 
-        return_occured
+        leave
     }
 
-    fn visit_stmt(self: &mut ReturnCheck, s: &mut Statement) -> bool {
+    fn visit_stmt(self: &mut ReturnCheck, s: &mut Statement) -> LeaveState {
         match s.stmt {
             If(_, ref mut tblock, ref mut eblock) => {
+                let tblock = self.visit_stmt(tblock);
                 let eblock = if eblock.is_some() {
                     self.visit_stmt(eblock.as_mut().unwrap())
                 } else {
-                    false
+                    LeaveState::new()
                 };
 
-                eblock && self.visit_stmt(tblock)
+                tblock.merge(eblock)
             }
 
             Block(ref mut stmts) => {
@@ -95,21 +168,28 @@ impl Visitor for ReturnCheck {
             }
 
             While(_, ref mut block) => {
-                self.visit_stmt(block)
+                let mut leave = self.visit_stmt(block);
+                leave.block = Leave::Not;
+
+                leave
             }
 
             Loop(ref mut block) => {
-                self.visit_stmt(block)
+                let mut leave = self.visit_stmt(block);
+                leave.block = Leave::Not;
+
+                leave
             }
 
-            Return(_) => true,
+            Return(_) => LeaveState::method(),
+            Break | Continue => LeaveState::block(),
 
-            _ => false
+            _ => LeaveState::new()
         }
     }
 
-    fn visit_expr(self: &mut ReturnCheck, _: &mut Expr) -> bool {
-        false
+    fn visit_expr(self: &mut ReturnCheck, _: &mut Expr) -> LeaveState {
+        LeaveState::new()
     }
 }
 
@@ -140,6 +220,16 @@ mod tests {
         err("fn f->int {}", ErrorCode::NoReturnValue, 1, 11);
         err("fn f->int {return 1;1;}", ErrorCode::UnreachableCode, 1, 21);
         parse("fn f->int {return 1;}");
+
+        err("fn f {break;}", ErrorCode::MisplacedBreak, 1, 7);
+        err("fn f {continue;}", ErrorCode::MisplacedContinue, 1, 7);
+    }
+
+    #[test]
+    fn check_while() {
+        parse("fn f->int {while true {return 1;}}");
+        parse("fn f->int {while true {break;} return 1;}");
+        parse("fn f->int {while true {continue;} return 1;}");
     }
 
     #[test]
