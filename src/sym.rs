b034537b15f7604510409fd9ffd564af8eb92cf3
Dominik Inführ <dominik.infuehr@gmail.com>
2015-05-01 16:52:45 +0200

    sym: added levels to symbol table

--- src/sym.rs
+++ src/sym.rs
@@ -1,19 +1,50 @@
 use std::collections::HashMap;
 
 pub struct SymbolTable {
-    map: HashMap<String,Sym>
+    levels: Vec<Level>
 }
 
 impl SymbolTable {
     pub fn new() -> SymbolTable {
-        SymbolTable { map: HashMap::new() }
+        SymbolTable { levels: vec![Level::new()] }
+    }
+
+    pub fn push_level(&mut self) {
+        self.levels.push(Level::new());
+    }
+
+    pub fn pop_level(&mut self) {
+        self.levels.pop()
     }
 
     pub fn get(&self, name: &str) -> Option<&Sym> {
-        self.map.get(name)
+        for level in self.levels.iter().rev() {
+            let found = level.get(name);
+            if found.is_some() { return found; }
+        }
+
+        None
     }
 
     pub fn insert(&mut self, name: String, sym: Sym) {
+        self.levels.last_mut().unwrap().insert(name, sym);
+    }
+}
+
+struct Level {
+    map: HashMap<String,Sym>
+}
+
+impl Level {
+    fn new() -> Level {
+        Level { map: HashMap::new() }
+    }
+
+    fn get(&self, name: &str) -> Option<&Sym> {
+        self.map.get(name)
+    }
+
+    fn insert(&mut self, name: String, sym: Sym) {
         let old = self.map.insert(name, sym);
         assert!(old.is_none());
     }
