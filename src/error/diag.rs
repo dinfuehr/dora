0292c88d71fe31b39e077ca108aa098e7dcbda67
Dominik Inführ <dominik.infuehr@gmail.com>
2015-08-26 22:42:01 -0700

    diag: now uses RefCell too

--- src/error/diag.rs
+++ src/error/diag.rs
@@ -1,24 +1,26 @@
+use std::cell::RefCell;
+
 use error::msg::Msg;
 use error::msg::MsgWithPos;
 
 use parser::lexer::position::Position;
 
 pub struct Diagnostic {
-    errors: Vec<MsgWithPos>,
+    errors: RefCell<Vec<MsgWithPos>>,
 }
 
 impl Diagnostic {
     pub fn new() -> Diagnostic {
         Diagnostic {
-            errors: Vec::new()
+            errors: RefCell::new(Vec::new())
         }
     }
 
-    pub fn report(&mut self, pos: Position, msg: Msg) {
-        self.errors.push(MsgWithPos::new(pos, msg));
+    pub fn report(&self, pos: Position, msg: Msg) {
+        self.errors.borrow_mut().push(MsgWithPos::new(pos, msg));
     }
 
     pub fn has_errors(&self) -> bool {
-        !self.errors.is_empty()
+        !self.errors.borrow().is_empty()
     }
 }
