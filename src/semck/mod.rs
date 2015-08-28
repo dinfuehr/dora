c971d843bee05975574d4377733e2e9bf33241cb
Dominik Inführ <dominik.infuehr@gmail.com>
2015-08-27 20:56:46 -0700

    typeck: moved add_builtin_types to this pass

--- src/semck/mod.rs
+++ src/semck/mod.rs
@@ -1,26 +1,9 @@
 use driver::ctxt::Context;
-use sym::BuiltinType;
-use sym::Sym::SymType;
 
 mod typeck;
 mod nameck;
 
 pub fn check(ctxt: &Context) {
-    add_builtin_types(ctxt);
-
     typeck::check(ctxt, ctxt.ast);
     nameck::check(ctxt, ctxt.ast);
-}
-
-fn add_builtin_types(ctxt: &Context) {
-    let mut sym = ctxt.sym.borrow_mut();
-
-    let name = ctxt.interner.intern("int".into());
-    assert!(sym.insert(name, SymType(BuiltinType::Int)).is_none());
-
-    let name = ctxt.interner.intern("bool".into());
-    assert!(sym.insert(name, SymType(BuiltinType::Bool)).is_none());
-
-    let name = ctxt.interner.intern("str".into());
-    assert!(sym.insert(name, SymType(BuiltinType::Str)).is_none());
 }
