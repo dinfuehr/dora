1b6e98170dcbde1eb74547dac241550ea1eb21e3
Dominik Inführ <dominik.infuehr@gmail.com>
2015-08-30 01:50:12 -0700

    typeck: added tests

--- src/semck/typeck.rs
+++ src/semck/typeck.rs
@@ -42,3 +42,27 @@ impl<'a, 'ast> Visitor<'ast> for TypeCheck<'a, 'ast> {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use error::msg::Msg;
+    use semck::tests::*;
+
+    #[test]
+    fn type_def_for_return_type() {
+        ok("fn a() -> int {}");
+        err("fn a() -> unknown {}", pos(1, 11), Msg::UnknownType("unknown".into()));
+    }
+
+    #[test]
+    fn type_def_for_param() {
+        ok("fn a(b: int) {}");
+        err("fn a(b: foo) {}", pos(1, 9), Msg::UnknownType("foo".into()));
+    }
+
+    #[test]
+    fn type_def_for_var() {
+        ok("fn a() { var a : int = 1; }");
+        err("fn a() { var a : test = 1; }", pos(1, 18), Msg::UnknownType("test".into()));
+    }
+}
