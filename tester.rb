8256c0e9c214027f6eb4e0af4027b0c2e559e48a
Dominik Inführ <dominik.infuehr@gmail.com>
2015-10-08 10:08:57 +0200

    added tester

---
+++ tester.rb
@@ -0,0 +1,52 @@
+#!/usr/bin/ruby
+
+require 'pathname'
+
+def run_tests
+  tests = 0
+  passed = 0
+  failed = 0
+
+  for file in Dir["tests/**/*.dora"]
+    file = Pathname.new(file)
+    tests += 1
+
+    print "test #{file} ... "
+
+    if run_test(file)
+      puts "ok"
+      passed += 1
+    else
+      puts "failed"
+      failed += 1
+    end
+  end
+
+  puts
+  puts "#{passed} tests passed; #{failed} tests failed"
+
+  tests == passed
+end
+
+def run_test(file)
+  ec = expected_exit_code(file)
+
+  system("target/debug/dora #{file}")
+  process = $?
+
+  process.exitstatus == ec
+end
+
+def expected_exit_code(file)
+  first_line = File.open(file, "r") do |f|
+    f.readline
+  end
+
+  if (m = first_line.match(/^\/\/ error: (\d+)$/)) != nil
+    m[1].to_i
+  else
+    0
+  end
+end
+
+exit run_tests ? 0 : 1
