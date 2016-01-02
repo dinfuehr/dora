2015d29b2efa758413381e3d69d8c57b299e16ef
Dominik Inführ <dominik.infuehr@gmail.com>
2016-01-02 09:05:58 +0100

    signal: print exception instruction

--- experiments/signal/ctxt.c
+++ experiments/signal/ctxt.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <signal.h>
 #include <unistd.h>
 #include <sys/mman.h>
@@ -36,13 +37,25 @@ void free_code(void *ptr) {
 }
 
 void handler(int signo, siginfo_t *info, void *context) {
-  printf("signal %d!\n", signo);
+  ucontext_t *ucontext = context;
+  mcontext_t *mcontext = &ucontext->uc_mcontext;
 
+  printf("signal %d!\n", signo);
+  uint8_t *xpc = (uint8_t*) mcontext->gregs[REG_RIP];
+
+  printf("exception program counter = %p: ", xpc);
+
+  for(int i=0; i<8; i++) {
+    printf("%x, ", xpc[i]);
+  }
+
+  printf("\n");
+
+  // mov eax, 4
   unsigned char code[] = { 0xb8, 4, 0, 0, 0, 0xc3 };
   ftype fct = alloc_code(code, sizeof(code));
 
-  ucontext_t *ucontext = context;
-  ucontext->uc_mcontext.gregs[REG_RIP] = (greg_t) fct;
+  mcontext->gregs[REG_RIP] = (greg_t) fct;
 }
 
 int main() {
@@ -56,8 +69,11 @@ int main() {
     perror("sigaction failed");
   }
 
+  // mov r10, [9]
   unsigned char code[] = { 0x4C, 0x8B, 0x14, 0x25, 9, 0, 0, 0 };
   ftype fct = alloc_code(code, sizeof(code));
+
+  printf("address of failure code = %p\n", fct);
 
   int res = fct();
   printf("res = %d\n", res);
