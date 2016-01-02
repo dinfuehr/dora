d937b5df447b21c0c07f1cf97a34db9276b8c0de
Dominik Inführ <dominik.infuehr@gmail.com>
2016-01-02 12:20:32 +0100

    signal: started trying to patch

--- experiments/signal/ctxt.c
+++ experiments/signal/ctxt.c
@@ -24,18 +24,6 @@ ftype alloc_code(const char *code, size_t len) {
   return ptr;
 }
 
-void free_code(void *ptr) {
-  long pagesize;
-
-  if ((pagesize = sysconf(_SC_PAGESIZE)) == -1) {
-    perror("sysconf(_SC_PAGESIZE) failed");
-  }
-
-  if (munmap(ptr, pagesize) == -1) {
-    perror("munmap failed");
-  }
-}
-
 void handler(int signo, siginfo_t *info, void *context) {
   ucontext_t *ucontext = context;
   mcontext_t *mcontext = &ucontext->uc_mcontext;
@@ -58,6 +46,17 @@ void handler(int signo, siginfo_t *info, void *context) {
   mcontext->gregs[REG_RIP] = (greg_t) fct;
 }
 
+void dump(const char *name, void *ptr, size_t len) {
+  uint8_t *byte_ptr = (uint8_t *) ptr;
+  printf("%s @ %p (%d bytes) = ", name, byte_ptr, len);
+
+  for(size_t i=0; i<len; i++) {
+    printf("%x, ", byte_ptr[i]);
+  }
+
+  printf("\n");
+}
+
 int main() {
   struct sigaction sa;
 
@@ -69,15 +68,38 @@ int main() {
     perror("sigaction failed");
   }
 
-  // mov r10, [9]
-  unsigned char code[] = { 0x4C, 0x8B, 0x14, 0x25, 9, 0, 0, 0 };
-  ftype fct = alloc_code(code, sizeof(code));
+  // int fct2 { .... }
+  // int fct1() { return fct2(); }
 
-  printf("address of failure code = %p\n", fct);
+  // compiler stub: mov r10, [9]
+  unsigned char fct2_stub[] = { 0x4C, 0x8B, 0x14, 0x25, 9, 0, 0, 0 };
+  ftype fct2 = alloc_code(fct2_stub, sizeof(fct2_stub));
 
-  int res = fct();
-  printf("res = %d\n", res);
+  dump("fct2_stub", fct2, sizeof(fct2_stub));
 
-  free_code(fct);
+  // movabs rax, 0x1122334455667788
+  // call [rax]
+  // ret
+  unsigned char fct1_code[] = {
+    0x48, 0xB8, 0x88, 0x77, 0x66, 0x55, 0x44, 0x33, 0x22, 0x11,
+    0xFF, 0x10,
+    0xC3
+  };
+
+  intptr_t *addr_ptr = (intptr_t *) ((char *) fct1_code + 2);
+  *(addr_ptr) = (intptr_t) fct2;
+
+  ftype fct1 = alloc_code(fct1_code, sizeof(fct1_code));
+  dump("fct1", fct1, sizeof(fct1_code));
+
+  printf("invoke fct1:\n");
+  int res1 = fct1();
+  printf("res = %d\n", res1);
+
+  // invoke fct a second time - stub should not be used anymore
+  printf("\ninvoke fct1 again:\n");
+  int res2 = fct1();
+  printf("res = %d\n", res2);
+
   return 0;
 }
