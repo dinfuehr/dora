956d606c01f7ec3c3997ed22d5457fc3ece502ae
Dominik Inführ <dominik.infuehr@gmail.com>
2016-01-04 16:21:45 +0100

    codegen: removed ByteBuffer

--- src/codegen/buffer.rs
+++ src/codegen/buffer.rs
@@ -1,53 +1,8 @@
 use byteorder::{LittleEndian, WriteBytesExt};
 
-#[derive(Copy, Clone, Debug, PartialEq, Eq)]
-pub struct Label(usize);
-
-#[derive(Debug)]
-struct ByteBuffer {
-    data: Vec<u8>,
-}
-
-impl ByteBuffer {
-    fn new() -> ByteBuffer {
-        ByteBuffer {
-            data: Vec::new()
-        }
-    }
-
-    pub fn data(self) -> Vec<u8> {
-        self.data
-    }
-
-    pub fn pos(&mut self) -> usize {
-        self.data.len()
-    }
-
-    pub fn emit_u8(&mut self, value: u8) {
-        self.data.write_u8(value).unwrap();
-    }
-
-    pub fn emit_u16(&mut self, value: u16) {
-        self.data.write_u16::<LittleEndian>(value).unwrap();
-    }
-
-    pub fn emit_u32(&mut self, value: u32) {
-        self.data.write_u32::<LittleEndian>(value).unwrap();
-    }
-
-    pub fn emit_u32_at(&mut self, value: u32, pos: usize) {
-        let mut slice = &mut self.data[pos..];
-        slice.write_u32::<LittleEndian>(value).unwrap();
-    }
-
-    pub fn emit_u64(&mut self, value: u64) {
-        self.data.write_u64::<LittleEndian>(value).unwrap();
-    }
-}
-
 #[derive(Debug)]
 pub struct Buffer {
-    buf: ByteBuffer,
+    data: Vec<u8>,
     labels: Vec<Option<usize>>,
     jumps: Vec<ForwardJump>,
 }
@@ -55,20 +10,20 @@ pub struct Buffer {
 impl Buffer {
     pub fn new() -> Buffer {
         Buffer {
-            buf: ByteBuffer::new(),
+            data: Vec::new(),
             labels: Vec::new(),
             jumps: Vec::new(),
         }
     }
 
-    pub fn pos(&mut self) -> usize {
-        self.buf.pos()
+    pub fn pos(&self) -> usize {
+        self.data.len()
     }
 
     pub fn finish(mut self) -> Vec<u8> {
         self.fix_forward_jumps();
 
-        self.buf.data()
+        self.data
     }
 
     fn fix_forward_jumps(&mut self) {
@@ -76,7 +31,8 @@ impl Buffer {
             let target = self.labels[jmp.to.0].expect("label not defined");
             let diff = (target - jmp.at - 4) as i32;
 
-            self.buf.emit_u32_at(diff as u32, jmp.at);
+            let mut slice = &mut self.data[jmp.at..];
+            slice.write_u32::<LittleEndian>(diff as u32).unwrap();
         }
     }
 
@@ -88,17 +44,19 @@ impl Buffer {
     }
 
     pub fn define_label(&mut self, lbl: Label) {
-        assert!(self.labels[lbl.0].is_none());
-        self.labels[lbl.0] = Some(self.buf.pos());
+        let lbl_idx = lbl.index();
+
+        assert!(self.labels[lbl_idx].is_none());
+        self.labels[lbl_idx] = Some(self.pos());
     }
 
     pub fn emit_label(&mut self, lbl: Label) {
-        let value = self.labels[lbl.0];
+        let value = self.labels[lbl.index()];
 
         match value {
             // backward jumps already know their target
             Some(idx) => {
-                let current = self.buf.pos() + 4;
+                let current = self.pos() + 4;
                 let target = idx;
 
                 let diff = -((current - target) as i32);
@@ -108,7 +66,7 @@ impl Buffer {
             // forward jumps do not know their target yet
             // we need to do this later...
             None => {
-                let pos = self.buf.pos();
+                let pos = self.pos();
                 self.emit_u32(0);
                 self.jumps.push(ForwardJump {
                     at: pos,
@@ -119,11 +77,11 @@ impl Buffer {
     }
 
     pub fn emit_u8(&mut self, value: u8) {
-        self.buf.emit_u8(value);
+        self.data.write_u8(value).unwrap();
     }
 
     pub fn emit_u32(&mut self, value: u32) {
-        self.buf.emit_u32(value);
+        self.data.write_u32::<LittleEndian>(value).unwrap();
     }
 }
 
@@ -131,6 +89,15 @@ impl Buffer {
 struct ForwardJump {
     at: usize,
     to: Label
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub struct Label(usize);
+
+impl Label {
+    pub fn index(&self) -> usize {
+        self.0
+    }
 }
 
 #[cfg(test)]
