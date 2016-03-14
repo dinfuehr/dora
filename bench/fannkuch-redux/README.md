4f2d4b28028a77f599d00de514a8dd8d00c432d8
Dominik Inführ <dominik.infuehr@gmail.com>
2016-03-14 21:46:19 +0100

    updated benchmark numbers

---
+++ bench/fannkuch-redux/README.md
@@ -0,0 +1,12 @@
+Notebook Fedora
+===============
+java (open jdk 8): 42s
+cacao: 90s
+dora: 180s
+perl: 940s
+
+PC Arch Linux
+=============
+Java: 40s
+Dora: 2m15s (=135s) (mit Array index checks: 160s)
+Perl: 19m50s
--- bench/fannkuch-redux/bench.sh
+++ bench/fannkuch-redux/bench.sh
@@ -2,13 +2,16 @@ echo "java"
 javac fannkuchredux.java
 for i in {1..5}; do time java fannkuchredux 12; done
 
-echo "cacao"
-javac -source 1.6 -target 1.6 fannkuchredux.java
-for i in {1..5}; do time cacao fannkuchredux 12; done
+#echo
+#echo "cacao"
+#javac -source 1.6 -target 1.6 fannkuchredux.java
+#for i in {1..5}; do time cacao fannkuchredux 12; done
 
+echo
 echo "dora"
 cargo build --release
-for i in {1..5}; do time cargo run fannkuchredux.dora 12; done
+for i in {1..5}; do time ../../target/release/dora fannkuchredux.dora 12; done
 
+echo
 echo "perl"
 for i in {1..5}; do time perl fannkuchredux.pl 12; done
