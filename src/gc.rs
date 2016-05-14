484cfc35b732295ab33042e3a8e3b2e2c05d8bfb
Dominik Inführ <dominik.infuehr@gmail.com>
2016-05-14 19:32:46 +0200

    GC: automatic collectio with threshold

--- src/gc.rs
+++ src/gc.rs
@@ -7,23 +7,40 @@ use ctxt::get_ctxt;
 use mem::ptr::Ptr;
 use object::Obj;
 
+const INITIAL_THRESHOLD: usize = 128;
+const USED_RATIO: f64 = 0.75;
+
 pub struct Gc {
     memory: Vec<Ptr>,
     bytes_allocated: usize,
+    threshold: usize,
 }
 
 impl Gc {
     pub fn new() -> Gc {
         Gc {
             memory: Vec::new(),
-            bytes_allocated: 0
+            bytes_allocated: 0,
+            threshold: INITIAL_THRESHOLD,
         }
     }
 
     pub fn alloc(&mut self, size: usize) -> Ptr {
         let ctxt = get_ctxt();
 
-        // TODO: invoke collect() if it is necessary
+        if self.bytes_allocated + size > self.threshold {
+            self.collect();
+
+            if (self.bytes_allocated + size) as f64 > self.threshold as f64 * USED_RATIO {
+                let saved_threshold = self.threshold;
+                self.threshold = (self.threshold as f64 / USED_RATIO) as usize;
+
+                if ctxt.args.flag_gc_dump {
+                    println!("GC: increase threshold from {} to {}",
+                             saved_threshold, self.threshold);
+                }
+            }
+        }
 
         let ptr = unsafe { libc::malloc(size) };
         unsafe { write_bytes(ptr, 0, size); }
@@ -33,8 +50,9 @@ impl Gc {
         self.bytes_allocated += size;
 
         if ctxt.args.flag_gc_dump {
-            println!("allocate {} bytes: {:x} (total: {} bytes, {} objects)", size,
-                ptr.raw() as usize, self.bytes_allocated, self.memory.len());
+            println!("GC: allocate {} bytes: {:x} (total {} bytes, threshold {}, {} objects)",
+                     size, ptr.raw() as usize,
+                     self.bytes_allocated, self.threshold, self.memory.len());
         }
 
         ptr
@@ -43,20 +61,19 @@ impl Gc {
     pub fn collect(&mut self) {
         let ctxt = get_ctxt();
         let rootset = get_rootset(ctxt);
-        let dump = ctxt.args.flag_gc_dump;
+
+        if ctxt.args.flag_gc_dump {
+            println!("GC: collect garbage");
+        }
 
         for &ptr in &self.memory {
             let obj = unsafe { &mut *(ptr.raw() as *mut Obj) };
             obj.header_mut().unmark();
-
-            if ctxt.args.flag_gc_dump {
-                println!("unmark {:x}", ptr.raw() as usize);
-            }
         }
 
-        mark_literals(dump);
-        mark_rootset(&rootset, dump);
-        sweep(self, dump);
+        mark_literals();
+        mark_rootset(&rootset);
+        sweep(self, ctxt.args.flag_gc_dump);
     }
 }
 
@@ -70,28 +87,26 @@ impl Drop for Gc {
     }
 }
 
-fn mark_literals(dump: bool) {
+fn mark_literals() {
     let ctxt = get_ctxt();
     let literals = ctxt.literals.lock().unwrap();
 
     for lit in literals.iter() {
-        mark_recursive(lit.raw() as usize, dump);
+        mark_recursive(lit.raw() as usize);
     }
 }
 
-fn mark_rootset(rootset: &Vec<usize>, dump: bool) {
+fn mark_rootset(rootset: &Vec<usize>) {
     for &root in rootset {
-        mark_recursive(root, dump);
+        mark_recursive(root);
     }
 }
 
-fn mark_recursive(ptr: usize, dump: bool) {
+fn mark_recursive(ptr: usize) {
     if ptr == 0 { return; }
     let obj = unsafe { &mut *(ptr as *mut Obj) };
 
     if !obj.header().is_marked() {
-        if dump { println!("mark {:x}", ptr); }
-
         obj.header_mut().mark();
         let class = obj.header().class();
 
@@ -101,11 +116,9 @@ fn mark_recursive(ptr: usize, dump: bool) {
                 let obj = unsafe { *(addr as *const usize) };
 
                 if obj == 0 { return; }
-                mark_recursive(obj, dump);
+                mark_recursive(obj);
             }
         }
-    } else {
-        println!("already marked {:x}", ptr);
     }
 }
 
@@ -117,10 +130,6 @@ fn sweep(gc: &mut Gc, dump: bool) {
         let obj = unsafe { &mut *(ptr.raw() as *mut Obj) };
 
         if !obj.header().is_marked() {
-            if dump {
-                println!("sweep {:x}", ptr.raw() as usize);
-            }
-
             let size = obj.size();
 
             unsafe {
@@ -131,6 +140,11 @@ fn sweep(gc: &mut Gc, dump: bool) {
 
             gc.bytes_allocated -= size;
             gc.memory.remove(i);
+
+            if dump {
+                println!("sweep {:x} with {} bytes", ptr.raw() as usize, size);
+            }
+
             continue;
         }
 
