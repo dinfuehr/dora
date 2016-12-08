1344e9a6d8ddde071da8c2605236640eb95253fa
Dominik Inführ <dominik.infuehr@gmail.com>
2016-12-08 22:06:04 +0100

    arm64: renamed aarch64 to arm64

--- experiments/aarch64/insn.s
+++ experiments/aarch64/insn.s
@@ -1,6 +1,8 @@
 .section .text
 .globl _start
 _start:
+        add w0, w0, w1
+        add x0, x0, x1
 	nop
 	ret
 
--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -4,7 +4,7 @@ pub use self::x64::*;
 #[cfg(target_arch = "x86_64")]
 pub mod x64;
 
-pub mod aarch64;
+pub mod arm64;
 
 #[derive(Copy, Clone, PartialEq, Eq, Debug)]
 pub struct Reg(u8);
