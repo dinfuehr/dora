62b3a71f104289fe493db203c4674f5e295a5303
Dominik Inführ <dominik.infuehr@gmail.com>
2017-02-01 20:24:02 +0100

    added comments

--- src/mem.rs
+++ src/mem.rs
@@ -3,10 +3,13 @@ use std::mem::size_of;
 
 use os;
 
+/// return pointer width: either 4 or 8
+/// (although only 64bit architectures are supported right now)
 pub fn ptr_width() -> i32 {
     size_of::<*const u8>() as i32
 }
 
+/// returns true if given value is a multiple of a page size.
 pub fn is_page_aligned(val: usize) -> bool {
     let align = os::page_size_bits();
 
@@ -28,6 +31,7 @@ fn test_is_page_aligned() {
     assert_eq!(true, is_page_aligned(3*p));
 }
 
+/// round the given value up to the nearest multiple of a page
 pub fn page_align(val: usize) -> usize {
     let align = os::page_size_bits();
 
@@ -47,6 +51,8 @@ fn test_page_align() {
     assert_eq!(2*p, page_align(p+1));
 }
 
+/// rounds the given value `val` up to the nearest multiple
+/// of `align`
 pub fn align(value: u32, align: u32) -> u32 {
     if align == 0 {
         return value;
@@ -55,6 +61,8 @@ pub fn align(value: u32, align: u32) -> u32 {
     ((value + align - 1) / align) * align
 }
 
+/// rounds the given value `val` up to the nearest multiple
+/// of `align`
 pub fn align_i32(value: i32, align: i32) -> i32 {
     if align == 0 {
         return value;
@@ -63,6 +71,8 @@ pub fn align_i32(value: i32, align: i32) -> i32 {
     ((value + align - 1) / align) * align
 }
 
+/// rounds the given value `val` up to the nearest multiple
+/// of `align`
 pub fn align_usize(value: usize, align: usize) -> usize {
     if align == 0 {
         return value;
@@ -71,10 +81,12 @@ pub fn align_usize(value: usize, align: usize) -> usize {
     ((value + align - 1) / align) * align
 }
 
+/// returns true if value fits into u8 (unsigned 8bits).
 pub fn fits_u8(value: i64) -> bool {
     0 <= value && value <= 255
 }
 
+/// returns true if value fits into i32 (signed 32bits).
 pub fn fits_i32(value: i64) -> bool {
     i32::MIN as i64 <= value && value <= i32::MAX as i64
 }
