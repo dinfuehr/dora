3de537906a7037687a14769d81934657f7402086
Dominik Inführ <dominik.infuehr@gmail.com>
2017-02-01 20:26:46 +0100

    added benchmark results

--- bench/binarytrees/bench.sh
+++ bench/binarytrees/bench.sh
@@ -1,16 +1,24 @@
+#!/bin/bash
 RUNS=3
+set -e
 
-echo "java"
+echo "java" 1>&2
 javac binarytrees.java
 for i in $(seq 1 $RUNS); do time java binarytrees 20; done
 
 echo
-echo "dora"
+echo "dora" 1>&2
 pushd ../..
 cargo build --release
 for i in $(seq 1 $RUNS); do time target/release/dora bench/binarytrees/binarytrees.dora 20; done
+for i in $(seq 1 $RUNS); do time target/release/dora --gc-copy --gc-copy-size=512m bench/binarytrees/binarytrees.dora 20; done
 popd
 
 echo
-echo "perl"
+echo "cacao" 1>&2
+javac  -source 1.6 -target 1.6 binarytrees.java
+for i in $(seq 1 $RUNS); do time /usr/local/cacao/bin/cacao -Xmx=512m binarytrees 20; done
+
+echo
+echo "perl" 1>&2
 for i in $(seq 1 $RUNS); do time perl binarytrees.pl 20; done
--- bench/fannkuchredux/bench.sh
+++ bench/fannkuchredux/bench.sh
@@ -1,16 +1,21 @@
+#!/bin/bash
 RUNS=3
 
-echo "java"
+echo "java" 1>&2
 javac fannkuchredux.java
 for i in $(seq 1 $RUNS); do time java fannkuchredux 12; done
 
 echo
-echo "dora"
+echo "dora" 1>&2
 pushd ../..
 cargo build --release
 for i in $(seq 1 $RUNS); do time target/release/dora bench/fannkuchredux/fannkuchredux.dora 12; done
 popd
 
+echo "cacao" 1>&2
+javac -source 1.6 -target 1.6 fannkuchredux.java
+for i in $(seq 1 $RUNS); do time /usr/local/cacao/bin/cacao fannkuchredux 12; done
+
 echo
-echo "perl"
+echo "perl" 1>&2
 for i in $(seq 1 $RUNS); do time perl fannkuchredux.pl 12; done
