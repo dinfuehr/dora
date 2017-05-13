ca7b234b479d2bba9b4af001ab002cf74b411b74
Dominik Inführ <dominik.infuehr@gmail.com>
2017-05-13 13:06:01 +0200

    started with info about regions

--- src/gc/swiper/mod.rs
+++ src/gc/swiper/mod.rs
@@ -3,12 +3,25 @@ use driver::cmd::Args;
 use gc::Collector;
 
 pub struct Swiper {
-
+    no_regions: usize,
+    region_size_bits: usize,
+    region_size: usize,
+    regions: Vec<RegionStatus>,
 }
 
 impl Swiper {
     pub fn new(args: &Args) -> Swiper {
-        Swiper {}
+        let heap_size = args.flag_heap_size
+            .map(|s| *s)
+            .unwrap_or(32 * 1024 * 1024);
+        let no_regions = heap_size / (1024 * 1024);
+
+        Swiper {
+            no_regions: no_regions,
+            region_size_bits: 20,
+            region_size: 1024 * 1024,
+            regions: vec![RegionStatus::Free, no_regions]
+        }
     }
 }
 
@@ -20,4 +33,11 @@ impl Collector for Swiper {
     fn collect(&self, ctxt: &Context) {
         unimplemented!();
     }
+}
+
+enum RegionStatus {
+    Young,
+    Old,
+    Huge,
+    Free,
 }
