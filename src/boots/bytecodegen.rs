ec879da39a0431854427326ca4e2f94130a32049
Rodrigo Bruno <rbruno@gsd.inesc-id.pt>
2018-12-08 21:38:50 +0100

    [boots] bytecodes now receive a Register instead of a usize.

--- src/boots/bytecodegen.rs
+++ src/boots/bytecodegen.rs
@@ -6,8 +6,11 @@ use dora_parser::ast::Stmt::*;
 use dora_parser::interner::Name;
 use dora_parser::ast::visit::Visitor;
 
+pub struct Register(usize);
+pub struct Label(usize);
+
 pub struct Context {
-    var_map: HashMap<Name, usize>,
+    var_map: HashMap<Name, Register>,
 }
 
 impl Context {
@@ -18,48 +21,48 @@ impl Context {
         }
     }
 
-    pub fn new_var(&mut self, var: Name, reg: usize) {
+    pub fn new_var(&mut self, var: Name, reg: Register) {
         self.var_map.insert(var, reg);
     }
 
-    pub fn get_reg(&self,var: Name) -> Option<&usize> {
+    pub fn get_reg(&self,var: Name) -> Option<&Register> {
         let reg = self.var_map.get(&var);
         reg
     }
 }
 
 pub struct LoopLabels {
-    cond: usize,
-    end: usize
+    cond: Label,
+    end: Label
 }
 
 pub enum Bytecode {
-    Add(usize),
-    BitwiseAnd(usize),
-    BitwiseOr(usize),
-    BitwiseXor(usize),
-    Dec,
-    Div(usize),
-    Inc,
-    Ldar(usize),
-    LdaSmi(u64),
+    Add(Register),
+    BitwiseAnd(Register),
+    BitwiseOr(Register),
+    BitwiseXor(Register),
+    Div(Register),
+    Ldar(Register),
+    LdaInt(u64),
     LdaZero,
     LogicalNot,
-    Star(usize),
-    JumpIfFalse(usize),
-    Jump(usize),
-    Mod(usize),
-    Mul(usize),
-    ShiftLeft(usize),
-    ShiftRight(usize),
-    Sub(usize),
+    Star(Register),
+    JumpIfFalse(Label),
+    Jump(Label),
+    Mod(Register),
+    Mul(Register),
+    Neg,
+    ShiftLeft(Register),
+    ShiftRight(Register),
+    Sub(Register),
     Return,
-    TestEqual(usize),
-    TestGreatherThan(usize),
-    TestGreatherThanOrEqual(usize),
-    TestLessThan(usize),
-    TestLessThanOrEqual(usize),
-    TestNotEqual(usize),
+    ReturnVoid,
+    TestEqual(Register),
+    TestGreatherThan(Register),
+    TestGreatherThanOrEqual(Register),
+    TestLessThan(Register),
+    TestLessThanOrEqual(Register),
+    TestNotEqual(Register),
 }
 
 pub struct BytecodeGen {
@@ -87,6 +90,10 @@ impl<'ast> visit::Visitor<'ast> for BytecodeGen {
         if let Some(ref block) = f.block {
             self.visit_stmt(block);
         }
+
+        if f.return_type.is_none() {
+            self.code.push(Bytecode::ReturnVoid);
+        }
     }
 }
 
@@ -105,7 +112,7 @@ impl BytecodeGen {
         self.visit_ast(ast);
     }
 
-    pub fn get_reg(&self, var: Name) -> Option<&usize> {
+    pub fn get_reg(&self, var: Name) -> Option<&Register> {
         for ctx in self.ctxs.iter() {
             let opt = ctx.get_reg(var);
             if opt.is_some() {
@@ -140,28 +147,28 @@ impl BytecodeGen {
         let reg = self.regs;
         let varid = stmt.name;
         self.regs += 1;
-        self.ctxs.last_mut().unwrap().new_var(varid as Name, reg);
+        self.ctxs.last_mut().unwrap().new_var(varid as Name, Register(reg));
 
         if let Some(ref expr) = stmt.expr {
             self.visit_expr(expr);
         } else {
             self.code.push(Bytecode::LdaZero);
         };
-        self.code.push(Bytecode::Star(reg));
+        self.code.push(Bytecode::Star(Register(reg)));
     }
 
     fn visit_stmt_while(&mut self, stmt: &StmtWhileType) {
         let cond_lbl = self.labels.len();
         let end_lbl = cond_lbl + 1;
-        self.loops.push(LoopLabels{ cond: cond_lbl, end: end_lbl});
+        self.loops.push(LoopLabels{ cond: Label(cond_lbl), end: Label(end_lbl)});
 
         self.labels.insert(cond_lbl, self.code.len());
         self.labels.insert(end_lbl, 0); // Just a place holder
 
         self.visit_expr(&stmt.cond);
-        self.code.push(Bytecode::JumpIfFalse(end_lbl));
+        self.code.push(Bytecode::JumpIfFalse(Label(end_lbl)));
         self.visit_stmt(&stmt.block);
-        self.code.push(Bytecode::Jump(cond_lbl));
+        self.code.push(Bytecode::Jump(Label(cond_lbl)));
         self.labels.insert(end_lbl, self.code.len());
         self.loops.pop();
     }
@@ -174,9 +181,9 @@ impl BytecodeGen {
         self.labels.insert(end_lbl, 0);  // Just a place holder
 
         self.visit_expr(&stmt.cond);
-        self.code.push(Bytecode::JumpIfFalse(else_lbl));
+        self.code.push(Bytecode::JumpIfFalse(Label(else_lbl)));
         self.visit_stmt(&stmt.then_block);
-        self.code.push(Bytecode::Jump(end_lbl));
+        self.code.push(Bytecode::Jump(Label(end_lbl)));
         self.labels.insert(else_lbl, self.code.len());
         self.visit_stmt(&stmt.then_block);
         self.labels.insert(end_lbl, self.code.len());
@@ -204,13 +211,13 @@ impl BytecodeGen {
     }
 
     fn visit_stmt_break(&mut self, stmt: &StmtBreakType) {
-        let lbl = self.loops.pop().unwrap();
-        self.code.push(Bytecode::Jump(lbl.end));
+        let Label(end) = self.loops.pop().unwrap().end;
+        self.code.push(Bytecode::Jump(Label(end)));
     }
 
     fn visit_stmt_continue(&mut self, stmt: &StmtContinueType) {
-        let lbl = self.loops.last().unwrap();
-        self.code.push(Bytecode::Jump(lbl.cond));
+        let Label(cond) = self.loops.last().unwrap().cond;
+        self.code.push(Bytecode::Jump(Label(cond)));
     }
 
     // TODO - implement other expressions
@@ -241,14 +248,14 @@ impl BytecodeGen {
     }
 
     fn visit_expr_lit_int(&mut self, lit: &ExprLitIntType) {
-        self.code.push(Bytecode::LdaSmi(lit.value));
+        self.code.push(Bytecode::LdaInt(lit.value));
     }
 
     fn visit_expr_un(&mut self, expr: &ExprUnType) {
         self.visit_expr(&expr.opnd);
         match expr.op {
-            UnOp::Plus => { self.code.push(Bytecode::Inc) },
-            UnOp::Neg => { self.code.push(Bytecode::Dec) },
+            UnOp::Plus => { },
+            UnOp::Neg => { self.code.push(Bytecode::Neg)},
             UnOp::Not => { self.code.push(Bytecode::LogicalNot) },
         }
     }
@@ -257,39 +264,39 @@ impl BytecodeGen {
         self.visit_expr(&expr.rhs);
         let rhs_reg = self.regs;
         self.regs += 1;
-        self.code.push(Bytecode::Star(rhs_reg));
+        self.code.push(Bytecode::Star(Register(rhs_reg)));
         self.visit_expr(&expr.lhs);
         match expr.op {
-            BinOp::Add => { self.code.push(Bytecode::Add(rhs_reg)) },
-            BinOp::Sub => { self.code.push(Bytecode::Sub(rhs_reg)) },
-            BinOp::Mul => { self.code.push(Bytecode::Mul(rhs_reg)) },
-            BinOp::Div => { self.code.push(Bytecode::Div(rhs_reg)) },
-            BinOp::Mod => { self.code.push(Bytecode::Mod(rhs_reg)) },
-            BinOp::BitOr => { self.code.push(Bytecode::BitwiseOr(rhs_reg)) },
-            BinOp::BitAnd => { self.code.push(Bytecode::BitwiseAnd(rhs_reg)) },
-            BinOp::BitXor => { self.code.push(Bytecode::BitwiseXor(rhs_reg)) },
-            BinOp::ShiftL => { self.code.push(Bytecode::ShiftLeft(rhs_reg)) },
-            BinOp::ShiftR => { self.code.push(Bytecode::ShiftRight(rhs_reg)) },
+            BinOp::Add => { self.code.push(Bytecode::Add(Register(rhs_reg))) },
+            BinOp::Sub => { self.code.push(Bytecode::Sub(Register(rhs_reg))) },
+            BinOp::Mul => { self.code.push(Bytecode::Mul(Register(rhs_reg))) },
+            BinOp::Div => { self.code.push(Bytecode::Div(Register(rhs_reg))) },
+            BinOp::Mod => { self.code.push(Bytecode::Mod(Register(rhs_reg))) },
+            BinOp::BitOr => { self.code.push(Bytecode::BitwiseOr(Register(rhs_reg))) },
+            BinOp::BitAnd => { self.code.push(Bytecode::BitwiseAnd(Register(rhs_reg))) },
+            BinOp::BitXor => { self.code.push(Bytecode::BitwiseXor(Register(rhs_reg))) },
+            BinOp::ShiftL => { self.code.push(Bytecode::ShiftLeft(Register(rhs_reg))) },
+            BinOp::ShiftR => { self.code.push(Bytecode::ShiftRight(Register(rhs_reg))) },
             // BinOp::Or => { },
             // BinOp::And => { },
             // BinOp::UnShiftR => { },
             BinOp::Cmp(op) => {
                 match op {
                     CmpOp::Eq => {
-                        self.code.push(Bytecode::TestEqual(rhs_reg)) },
+                        self.code.push(Bytecode::TestEqual(Register(rhs_reg))) },
                     CmpOp::Ne => {
-                        self.code.push(Bytecode::TestNotEqual(rhs_reg)) },
+                        self.code.push(Bytecode::TestNotEqual(Register(rhs_reg))) },
                     CmpOp::Lt => {
-                        self.code.push(Bytecode::TestLessThan(rhs_reg)) },
+                        self.code.push(Bytecode::TestLessThan(Register(rhs_reg))) },
                     CmpOp::Le => {
                         self.code.push(
-                            Bytecode::TestLessThanOrEqual(rhs_reg)) },
+                            Bytecode::TestLessThanOrEqual(Register(rhs_reg))) },
                     CmpOp::Ge => {
                         self.code.push(
-                            Bytecode::TestGreatherThan(rhs_reg)) },
+                            Bytecode::TestGreatherThan(Register(rhs_reg))) },
                     CmpOp::Gt => {
                         self.code.push(
-                            Bytecode::TestGreatherThanOrEqual (rhs_reg)) },
+                            Bytecode::TestGreatherThanOrEqual (Register(rhs_reg))) },
                     // CmpOp::Is => { },
                     // CmpOp::IsNot => { },
                     _ => unimplemented!(),
@@ -304,15 +311,15 @@ impl BytecodeGen {
         self.visit_expr(&expr.rhs);
         match *expr.lhs {
             ExprIdent(ref assign) => {
-                let reg = *self.get_reg(assign.name).unwrap();
-                self.code.push(Bytecode::Star(reg));
+                let Register(reg) = *self.get_reg(assign.name).unwrap();
+                self.code.push(Bytecode::Star(Register(reg)));
             },
             _ => unimplemented!(),
         }
     }
 
     fn visit_expr_ident(&mut self, ident: &ExprIdentType) {
-        let reg = *self.get_reg(ident.name).unwrap();
-        self.code.push(Bytecode::Ldar(reg));
+        let Register(reg) = *self.get_reg(ident.name).unwrap();
+        self.code.push(Bytecode::Ldar(Register(reg)));
     }
 }
