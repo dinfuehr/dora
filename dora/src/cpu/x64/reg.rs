56b3e4b05b543d4c58c971ed9cf8071cf4d986d8
Dominik Inführ <dominik.infuehr@gmail.com>
2020-01-30 22:43:09 +0100

    do not use windows callee-saved register as scratch register

--- dora/src/cpu/x64/reg.rs
+++ dora/src/cpu/x64/reg.rs
@@ -8,7 +8,7 @@ pub static REG_PARAMS: [Reg; 6] = [RDI, RSI, RDX, RCX, R8, R9];
 #[cfg(target_family = "windows")]
 pub static REG_PARAMS: [Reg; 4] = [RCX, RDX, R8, R9];
 
-pub static SCRATCH: [Reg; 3] = [R9, R8, RDI];
+pub static SCRATCH: [Reg; 4] = [RCX, RDX, R8, R9];
 
 pub const REG_RESULT: Reg = RAX;
 pub const REG_TMP1: Reg = R10;
