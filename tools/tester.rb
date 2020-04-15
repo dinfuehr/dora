13e522b8785f95eb75587f1649fbfdafea86ab70
Horst Zahradnik <horst.zahradnik@gmail.com>
2020-04-15 12:02:16 +0200

    test-runner: replaced test spawn with popen3 variant

--- tools/tester.rb
+++ tools/tester.rb
@@ -29,63 +29,7 @@ $ARGS.delete_if do |arg|
 end
 
 class TestUtility
-  # https://gist.github.com/pasela/9392115
-  # Capture the standard output and the standard error of a command.
-  # Almost same as Open3.capture3 method except for timeout handling and return value.
-  # See Open3.capture3.
-  #
-  #   result = capture3_with_timeout([env,] cmd... [, opts])
-  #
-  # The arguments env, cmd and opts are passed to Process.spawn except
-  # opts[:stdin_data], opts[:binmode], opts[:timeout], opts[:signal]
-  # and opts[:kill_after].  See Process.spawn.
-  #
-  # If opts[:stdin_data] is specified, it is sent to the command's standard input.
-  #
-  # If opts[:binmode] is true, internal pipes are set to binary mode.
-  #
-  # If opts[:timeout] is specified, SIGTERM is sent to the command after specified seconds.
-  #
-  # If opts[:signal] is specified, it is used instead of SIGTERM on timeout.
-  #
-  # If opts[:kill_after] is specified, also send a SIGKILL after specified seconds.
-  # it is only sent if the command is still running after the initial signal was sent.
-  #
-  # The return value is a Hash as shown below.
-  #
-  #   {
-  #     :pid     => PID of the command,
-  #     :status  => Process::Status of the command,
-  #     :stdout  => the standard output of the command,
-  #     :stderr  => the standard error of the command,
-  #     :timeout => whether the command was timed out,
-  #   }
-
-  def self.capture3_with_timeout(*cmd)
-    spawn_opts = Hash === cmd.last ? cmd.pop.dup : {}
-    opts = {
-      :stdin_data => spawn_opts.delete(:stdin_data) || "",
-      :binmode    => spawn_opts.delete(:binmode) || false,
-      :timeout    => spawn_opts.delete(:timeout),
-      :signal     => spawn_opts.delete(:signal) || :TERM,
-      :kill_after => spawn_opts.delete(:kill_after),
-    }
-
-    in_r,  in_w  = IO.pipe
-    out_r, out_w = IO.pipe
-    err_r, err_w = IO.pipe
-    in_w.sync = true
-
-    if opts[:binmode]
-      in_w.binmode
-      out_r.binmode
-      err_r.binmode
-    end
-
-    spawn_opts[:in]  = in_r
-    spawn_opts[:out] = out_w
-    spawn_opts[:err] = err_w
-
+  def self.spawn_with_timeout(cmd, timeout)
     result = {
       :pid     => nil,
       :status  => nil,
@@ -96,39 +40,31 @@ class TestUtility
 
     out_reader = nil
     err_reader = nil
-    wait_thr = nil
 
     begin
-      Timeout.timeout(opts[:timeout]) do
-        result[:pid] = spawn(*cmd, spawn_opts)
-        wait_thr = Process.detach(result[:pid])
-        in_r.close
-        out_w.close
-        err_w.close
+      Open3.popen3(cmd) do | stdin, stdout, stderr, wait_thr |
+        Timeout.timeout(timeout) do
+          result[:pid] = wait_thr.pid
 
-        out_reader = Thread.new { out_r.read }
-        err_reader = Thread.new { err_r.read }
+          stdin.close
+          out_reader = Thread.new { stdout.read }
+          err_reader = Thread.new { stderr.read }
 
-        in_w.write opts[:stdin_data]
-        in_w.close
-
-        result[:status] = wait_thr.value
-      end
-    rescue Timeout::Error
-      result[:timeout] = true
-      pid = spawn_opts[:pgroup] ? -result[:pid] : result[:pid]
-      Process.kill(opts[:signal], pid)
-      if opts[:kill_after]
-        unless wait_thr.join(opts[:kill_after])
-          Process.kill(:KILL, pid)
+          result[:status] = wait_thr.value
         end
+      rescue Timeout::Error
+        result[:timeout] = true
+        out_reader = out_reader.kill
+        err_reader = err_reader.kill
+
+        Process.kill(:TERM, result[:pid])
+      ensure
+        result[:status] = wait_thr.value if wait_thr
+        result[:stdout] = out_reader.value if out_reader
+        result[:stderr] = err_reader.value if err_reader
+        stdout.close unless stdout.closed?
+        stderr.close unless stderr.closed?
       end
-    ensure
-      result[:status] = wait_thr.value if wait_thr
-      result[:stdout] = out_reader.value if out_reader
-      result[:stderr] = err_reader.value if err_reader
-      out_r.close unless out_r.closed?
-      err_r.close unless err_r.closed?
     end
 
     result
@@ -213,14 +149,14 @@ class TestCase
   def run_test(optional_vm_args, mutex)
     temp_out = Tempfile.new("dora-test-runner")
     cmdline = "#{binary} #{vm_args} #{optional_vm_args} #{test_file} #{args}"
-    process_result = TestUtility.capture3_with_timeout(cmdline, :timeout => self.timeout)
+    process_result = TestUtility.spawn_with_timeout(cmdline, self.timeout)
     result = check_test_run_result(process_result)
     if $no_capture || result != true
       mutex.synchronize do
         puts "#==== STDOUT"
-        puts process_result[:stdout] unless process_result[:stdout].empty?
+        puts process_result[:stdout] unless process_result[:stdout] == nil || process_result[:stdout].empty?
         puts "#==== STDERR"
-        puts process_result[:stderr] unless process_result[:stderr].empty?
+        puts process_result[:stderr] unless process_result[:stderr] == nil || process_result[:stderr].empty?
         puts "RUN: #{cmdline}"
         STDOUT.flush
       end
