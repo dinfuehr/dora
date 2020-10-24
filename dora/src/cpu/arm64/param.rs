3656295f2140b850491853225dc2ab3c06f31fb4
Dominik Inführ <dominik.infuehr@gmail.com>
2020-10-24 20:49:58 +0200

    fix build on arm64

--- dora/src/cpu/arm64/param.rs
+++ dora/src/cpu/arm64/param.rs
@@ -1,7 +1,7 @@
-use crate::ty::BuiltinType;
+use crate::ty::SourceType;
 
 pub static PARAM_OFFSET: i32 = 16;
 
-pub fn next_param_offset(param_offset: i32, _: BuiltinType) -> i32 {
+pub fn next_param_offset(param_offset: i32, _: SourceType) -> i32 {
     param_offset + 8
 }
