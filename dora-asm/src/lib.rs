40daa9b358c90fc608ca3f5e0899a299c968d29e
Dominik Inführ <dominik.infuehr@gmail.com>
2021-04-16 20:01:31 +0200

    asm: introduce AssemblerBuffer

--- dora-asm/src/lib.rs
+++ dora-asm/src/lib.rs
@@ -18,40 +18,101 @@ use std::convert::TryInto;
 pub struct Label(usize);
 
 pub struct Assembler {
-    code: Vec<u8>,
-    labels: Vec<Option<u32>>,
     unresolved_jumps: Vec<(u32, Label, JumpKind)>,
-    position: usize,
+    buffer: AssemblerBuffer,
 }
 
 impl Assembler {
     pub fn new() -> Assembler {
         Assembler {
-            code: Vec::new(),
-            labels: Vec::new(),
             unresolved_jumps: Vec::new(),
-            position: 0,
+            buffer: AssemblerBuffer::new(),
         }
     }
 
     pub fn create_label(&mut self) -> Label {
+        self.buffer.create_label()
+    }
+
+    pub fn create_and_bind_label(&mut self) -> Label {
+        self.buffer.create_and_bind_label()
+    }
+
+    pub fn bind_label(&mut self, lbl: Label) {
+        self.buffer.bind_label(lbl);
+    }
+
+    pub fn bind_label_to(&mut self, lbl: Label, offset: u32) {
+        self.buffer.bind_label_to(lbl, offset);
+    }
+
+    fn offset(&self, lbl: Label) -> Option<u32> {
+        self.buffer.offset(lbl)
+    }
+
+    pub fn finalize(mut self) -> Vec<u8> {
+        self.resolve_jumps();
+        self.buffer.code
+    }
+
+    pub fn position(&self) -> usize {
+        self.buffer.position()
+    }
+
+    pub fn set_position(&mut self, pos: usize) {
+        self.buffer.set_position(pos);
+    }
+
+    pub fn set_position_end(&mut self) {
+        self.buffer.set_position_end();
+    }
+
+    pub fn emit_u8(&mut self, value: u8) {
+        self.buffer.emit_u8(value);
+    }
+
+    pub fn emit_u32(&mut self, value: u32) {
+        self.buffer.emit_u32(value);
+    }
+
+    pub fn emit_u64(&mut self, value: u64) {
+        self.buffer.emit_u64(value);
+    }
+}
+
+struct AssemblerBuffer {
+    code: Vec<u8>,
+    position: usize,
+    labels: Vec<Option<u32>>,
+}
+
+impl AssemblerBuffer {
+    fn new() -> AssemblerBuffer {
+        AssemblerBuffer {
+            code: Vec::new(),
+            position: 0,
+            labels: Vec::new(),
+        }
+    }
+
+    fn create_label(&mut self) -> Label {
         self.labels.push(None);
 
         Label(self.labels.len() - 1)
     }
 
-    pub fn create_and_bind_label(&mut self) -> Label {
+    fn create_and_bind_label(&mut self) -> Label {
         self.labels.push(Some(self.position().try_into().unwrap()));
         Label(self.labels.len() - 1)
     }
 
-    pub fn bind_label(&mut self, lbl: Label) {
+    fn bind_label(&mut self, lbl: Label) {
         let Label(idx) = lbl;
         assert!(self.labels[idx].is_none());
         self.labels[idx] = Some(self.position().try_into().unwrap());
     }
 
-    pub fn bind_label_to(&mut self, lbl: Label, offset: u32) {
+    fn bind_label_to(&mut self, lbl: Label, offset: u32) {
         let Label(idx) = lbl;
         assert!(self.labels[idx].is_none());
         self.labels[idx] = Some(offset);
@@ -62,24 +123,19 @@ impl Assembler {
         self.labels[idx]
     }
 
-    pub fn finalize(mut self) -> Vec<u8> {
-        self.resolve_jumps();
-        self.code
-    }
-
-    pub fn position(&self) -> usize {
+    fn position(&self) -> usize {
         self.position
     }
 
-    pub fn set_position(&mut self, pos: usize) {
+    fn set_position(&mut self, pos: usize) {
         self.position = pos;
     }
 
-    pub fn set_position_end(&mut self) {
+    fn set_position_end(&mut self) {
         self.position = self.code.len();
     }
 
-    pub fn emit_u8(&mut self, value: u8) {
+    fn emit_u8(&mut self, value: u8) {
         if self.position == self.code.len() {
             self.code.push(value);
         } else {
@@ -88,7 +144,7 @@ impl Assembler {
         self.position += 1;
     }
 
-    pub fn emit_u32(&mut self, value: u32) {
+    fn emit_u32(&mut self, value: u32) {
         if self.position == self.code.len() {
             self.code.write_u32::<LittleEndian>(value).unwrap()
         } else {
@@ -99,7 +155,7 @@ impl Assembler {
         self.position += 4;
     }
 
-    pub fn emit_u64(&mut self, value: u64) {
+    fn emit_u64(&mut self, value: u64) {
         if self.position == self.code.len() {
             self.code.write_u64::<LittleEndian>(value).unwrap()
         } else {
