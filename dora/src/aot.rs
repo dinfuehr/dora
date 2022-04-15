4ba50286e16749c25a365b7331813860821d7f7b
Dominik Inführ <dominik.infuehr@gmail.com>
2022-04-15 12:16:33 +0200

    aot: add function for emitting functions

--- dora/src/aot.rs
+++ dora/src/aot.rs
@@ -5,6 +5,7 @@ use crate::cannon::{self, CompilationFlags};
 use crate::compiler::dora_entry_stub;
 use crate::language::sem_analysis::{FctDefinition, FctDefinitionId};
 use crate::language::ty::SourceTypeArray;
+use crate::masm::CodeDescriptor;
 use crate::vm::SemAnalysis;
 
 pub fn build(sa: &SemAnalysis, main_fct_id: FctDefinitionId) {
@@ -18,28 +19,33 @@ fn write(sa: &SemAnalysis, fct: &FctDefinition) -> io::Result<()> {
     let mut file = File::create("program.s")?;
     writeln!(&mut file, ".text")?;
 
-    writeln!(&mut file, "\t.globl _dora_main")?;
-    writeln!(&mut file, "\t.p2align 4")?;
-    writeln!(&mut file, "_dora_main:")?;
     let code_descriptor = cannon::compile(
         sa,
         &*fct,
         &SourceTypeArray::empty(),
         CompilationFlags::aot(),
     );
-    write_binary_buffer(&mut file, &code_descriptor.code)?;
+    write_fct(&mut file, "_dora_main", code_descriptor)?;
 
-    writeln!(&mut file, "\t.globl _dora_entry_stub")?;
-    writeln!(&mut file, "\t.p2align 4")?;
-    writeln!(&mut file, "_dora_entry_stub:")?;
     let code_descriptor = dora_entry_stub::generate(sa);
-    write_binary_buffer(&mut file, &code_descriptor.code)?;
+    write_fct(&mut file, "_dora_entry_stub", code_descriptor)?;
+
+    Ok(())
+}
+
+fn write_fct(file: &mut File, name: &str, code_descriptor: CodeDescriptor) -> io::Result<()> {
+    writeln!(file, "\t.globl {}", name)?;
+    writeln!(file, "\t.p2align 4")?;
+    writeln!(file, "{}:", name)?;
+    writeln!(file, "\t.cfi_startproc")?;
+    write_binary_buffer(file, &code_descriptor.code)?;
+    writeln!(file, "\t.cfi_endproc")?;
 
     Ok(())
 }
 
 fn write_binary_buffer(file: &mut File, buffer: &[u8]) -> io::Result<()> {
-    write!(file, ".byte {:x}", buffer.first().expect("empty buffer"))?;
+    write!(file, "\t.byte {:x}", buffer.first().expect("empty buffer"))?;
 
     for &value in buffer.iter().skip(1) {
         write!(file, ", 0x{:x}", value)?;
