6dc06811b70c0099654f59e47a6b9f8fd87bf553
Dominik Inführ <dominik.infuehr@gmail.com>
2022-04-16 12:42:58 +0200

    aot: discover reachable functions

--- dora/src/aot.rs
+++ dora/src/aot.rs
@@ -1,6 +1,8 @@
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::{self, Write};
 
+use crate::bytecode::{BytecodeInstruction, BytecodeReader, ConstPoolEntry};
 use crate::cannon::{self, CompilationFlags};
 use crate::compiler::dora_entry_stub;
 use crate::language::sem_analysis::{FctDefinition, FctDefinitionId};
@@ -9,7 +11,7 @@ use crate::masm::CodeDescriptor;
 use crate::vm::SemAnalysis;
 
 pub fn build(sa: &SemAnalysis, main_fct_id: FctDefinitionId) {
-    let reachable_fcts = discover_reachable_functions(main_fct_id);
+    let reachable_fcts = discover_reachable_functions(sa, main_fct_id);
 
     write_file(sa, &reachable_fcts).expect("write failed");
 }
@@ -106,7 +108,72 @@ fn write_binary_buffer(file: &mut File, buffer: &[u8]) -> io::Result<()> {
 }
 
 fn discover_reachable_functions(
+    sa: &SemAnalysis,
     main_fct_id: FctDefinitionId,
 ) -> Vec<(FctDefinitionId, SourceTypeArray)> {
-    vec![(main_fct_id, SourceTypeArray::empty())]
+    let mut fcts = ReachableFunction::new(sa);
+    fcts.find_all(main_fct_id);
+    fcts.reachable_fcts
+}
+
+struct ReachableFunction<'a> {
+    sa: &'a SemAnalysis,
+    worklist: Vec<(FctDefinitionId, SourceTypeArray)>,
+    visited: HashSet<(FctDefinitionId, SourceTypeArray)>,
+    reachable_fcts: Vec<(FctDefinitionId, SourceTypeArray)>,
+}
+
+impl<'a> ReachableFunction<'a> {
+    fn new(sa: &'a SemAnalysis) -> ReachableFunction<'a> {
+        ReachableFunction {
+            sa,
+            worklist: Vec::new(),
+            visited: HashSet::new(),
+            reachable_fcts: Vec::new(),
+        }
+    }
+
+    fn find_all(&mut self, main_fct_id: FctDefinitionId) {
+        self.push_worklist(main_fct_id, SourceTypeArray::empty());
+
+        while let Some((fct_id, type_params)) = self.worklist.pop() {
+            self.reachable_fcts.push((fct_id, type_params.clone()));
+            self.discover_outgoing_calls(fct_id, type_params);
+        }
+    }
+
+    fn discover_outgoing_calls(&mut self, fct_id: FctDefinitionId, _type_params: SourceTypeArray) {
+        let fct = self.sa.fcts.idx(fct_id);
+        let fct = fct.read();
+
+        let bytecode_fct = fct.bytecode.as_ref().expect("bytecode missing");
+
+        let mut reader = BytecodeReader::new(bytecode_fct.code());
+
+        while let Some(inst) = reader.next() {
+            match inst {
+                BytecodeInstruction::InvokeStatic { fct: idx, dest: _ }
+                | BytecodeInstruction::InvokeStaticVoid { fct: idx }
+                | BytecodeInstruction::InvokeDirect { fct: idx, dest: _ }
+                | BytecodeInstruction::InvokeDirectVoid { fct: idx } => {
+                    let entry = bytecode_fct.const_pool(idx);
+                    match entry {
+                        ConstPoolEntry::Fct(fct_id, type_params) => {
+                            self.push_worklist(*fct_id, type_params.clone());
+                        }
+
+                        _ => unreachable!(),
+                    }
+                }
+
+                _ => {}
+            }
+        }
+    }
+
+    fn push_worklist(&mut self, fct_id: FctDefinitionId, type_params: SourceTypeArray) {
+        if self.visited.insert((fct_id, type_params.clone())) {
+            self.worklist.push((fct_id, type_params));
+        }
+    }
 }
