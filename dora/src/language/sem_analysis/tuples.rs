e6c578d119884da7db1f881e81b13001d2330c5e
Dominik Inführ <dominik.infuehr@gmail.com>
2022-04-18 21:55:55 +0200

    use SourceTypeArray inside Tuples

--- dora/src/language/sem_analysis/tuples.rs
+++ dora/src/language/sem_analysis/tuples.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 use std::sync::Arc;
 
 use crate::language::sem_analysis::SemAnalysis;
-use crate::language::ty::SourceType;
+use crate::language::ty::{SourceType, SourceTypeArray};
 
 #[derive(Copy, Clone, PartialEq, Eq, Hash, Debug)]
 pub struct TupleId(u32);
@@ -20,8 +20,8 @@ impl TupleId {
 }
 
 pub struct Tuples {
-    all: Vec<Arc<Vec<SourceType>>>,
-    map: HashMap<Arc<Vec<SourceType>>, TupleId>,
+    all: Vec<SourceTypeArray>,
+    map: HashMap<SourceTypeArray, TupleId>,
 }
 
 impl Tuples {
@@ -33,23 +33,27 @@ impl Tuples {
     }
 
     fn get_subtypes(&self, id: TupleId) -> Arc<Vec<SourceType>> {
-        self.all[id.0 as usize].clone()
+        let source_type_array = self.all[id.0 as usize].clone();
+
+        match source_type_array {
+            SourceTypeArray::Empty => Arc::new(Vec::new()),
+            SourceTypeArray::List(data) => data,
+        }
     }
 }
 
 pub fn ensure_tuple(sa: &SemAnalysis, args: Vec<SourceType>) -> TupleId {
-    let args = Arc::new(args);
-
     let mut tuples = sa.tuples.lock();
+    let source_type_array = SourceTypeArray::with(args);
 
-    if let Some(&tuple_id) = tuples.map.get(&args) {
+    if let Some(&tuple_id) = tuples.map.get(&source_type_array) {
         return tuple_id;
     }
 
-    tuples.all.push(args.clone());
+    tuples.all.push(source_type_array.clone());
 
     let id = TupleId((tuples.all.len() - 1).try_into().unwrap());
-    tuples.map.insert(args, id);
+    tuples.map.insert(source_type_array, id);
 
     id
 }
