7b7215cf080251e953d0e58ef3ddb58f6a635053
Dominik Inführ <dominik.infuehr@gmail.com>
2022-12-27 11:04:32 +0100

    allow readFileAsString to fail gracefully

--- dora/src/stdlib/io.rs
+++ dora/src/stdlib/io.rs
@@ -3,13 +3,22 @@ use std::{fs, path::PathBuf};
 
 use crate::handle::{handle_scope, Handle};
 use crate::object::{Ref, Str};
+use crate::threads::parked_scope;
 use crate::vm::get_vm;
 
 pub extern "C" fn read_file_as_string(name: Handle<Str>) -> Ref<Str> {
-    let path = PathBuf::from_str(name.content_utf8()).expect("should be a valid path");
-    let content = fs::read_to_string(path).expect("can't read file");
-
     handle_scope(|| {
+        let path = PathBuf::from_str(name.content_utf8());
+        if path.is_err() {
+            return Ref::null();
+        }
+        let path = path.unwrap();
+        let content = parked_scope(|| fs::read_to_string(path));
+        if content.is_err() {
+            return Ref::null();
+        }
+        let content = content.unwrap();
+
         let vm = get_vm();
         Str::from_buffer(vm, content.as_bytes())
     })
