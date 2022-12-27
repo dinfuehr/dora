61aa99ee54b671e1b3affa93160a7b21f3d0f4bb
Dominik Inführ <dominik.infuehr@gmail.com>
2022-12-27 11:24:19 +0100

    io: introduce readFileAsBytes function

--- dora/src/language/stdlib.rs
+++ dora/src/language/stdlib.rs
@@ -1525,6 +1525,13 @@ pub fn resolve_internal_functions(sa: &mut SemAnalysis) {
         stdlib::io::read_file_as_string as *const u8,
     );
 
+    native_fct(
+        sa,
+        stdlib_id,
+        "io::readFileAsBytes",
+        stdlib::io::read_file_as_bytes as *const u8,
+    );
+
     let fct_id = intrinsic_method(sa, stdlib_id, "Option", "isNone", Intrinsic::OptionIsNone);
     sa.known.functions.option_is_none = Some(fct_id);
     let fct_id = intrinsic_method(sa, stdlib_id, "Option", "isSome", Intrinsic::OptionIsSome);
--- dora/src/stdlib/io.rs
+++ dora/src/stdlib/io.rs
@@ -1,8 +1,10 @@
+use std::fs::File;
+use std::io::Read;
 use std::str::FromStr;
 use std::{fs, path::PathBuf};
 
 use crate::handle::{handle_scope, Handle};
-use crate::object::{Ref, Str};
+use crate::object::{byte_array_from_buffer, Ref, Str, UInt8Array};
 use crate::threads::parked_scope;
 use crate::vm::get_vm;
 
@@ -21,5 +23,36 @@ pub extern "C" fn read_file_as_string(name: Handle<Str>) -> Ref<Str> {
 
         let vm = get_vm();
         Str::from_buffer(vm, content.as_bytes())
+    })
+}
+
+pub extern "C" fn read_file_as_bytes(name: Handle<Str>) -> Ref<UInt8Array> {
+    handle_scope(|| {
+        let path = PathBuf::from_str(name.content_utf8());
+        if path.is_err() {
+            return Ref::null();
+        }
+        let path = path.unwrap();
+        let content: Option<Vec<u8>> = parked_scope(|| {
+            let f = File::open(&path);
+            if f.is_err() {
+                return None;
+            }
+            let mut f = f.unwrap();
+            let mut buffer = Vec::new();
+            match f.read_to_end(&mut buffer) {
+                Ok(_) => Some(buffer),
+                Err(_) => None,
+            }
+        });
+
+        if content.is_none() {
+            return Ref::null();
+        }
+
+        let content = content.unwrap();
+
+        let vm = get_vm();
+        byte_array_from_buffer(vm, &content)
     })
 }
