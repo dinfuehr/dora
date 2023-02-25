172cdcc0223926e04490fb9e8fa8a5afcc79bdbc
Dominik Inführ <dominik.infuehr@gmail.com>
2023-02-25 20:57:30 +0100

    disassembler: fix build when disabled

--- dora/src/disassembler/none.rs
+++ dora/src/disassembler/none.rs
@@ -1,6 +1,6 @@
+use crate::bytecode::BytecodeTypeArray;
 use crate::driver::cmd::AsmSyntax;
 use crate::language::sem_analysis::FctDefinition;
-use crate::language::ty::SourceTypeArray;
 use crate::vm::{Code, VM};
 
 pub fn supported() -> bool {
@@ -10,7 +10,7 @@ pub fn supported() -> bool {
 pub fn disassemble(
     _vm: &VM,
     _fct: &FctDefinition,
-    _type_params: &SourceTypeArray,
+    _type_params: &BytecodeTypeArray,
     _code: &Code,
     _asm_syntax: AsmSyntax,
 ) {
