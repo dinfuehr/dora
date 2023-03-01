98fed7f77e00dfd8789b66b9fe96a2871db77ea5
Dominik Inführ <dinfuehr@chromium.org>
2023-03-01 20:14:37 +0100

    vm: use formatter for BytecodeTypePrinter

--- dora/src/vm/ty.rs
+++ dora/src/vm/ty.rs
@@ -44,94 +44,113 @@ struct BytecodeTypePrinter<'a> {
 
 impl<'a> BytecodeTypePrinter<'a> {
     fn string(&self) -> String {
-        self.name(self.ty.clone())
+        format!("{}", self)
     }
 
-    fn name(&self, ty: BytecodeType) -> String {
+    fn name(&self, ty: &BytecodeType, fmt: &mut std::fmt::Formatter) -> std::fmt::Result {
         match ty {
-            BytecodeType::Unit => "()".into(),
-            BytecodeType::UInt8 => "UInt8".into(),
-            BytecodeType::Char => "Char".into(),
-            BytecodeType::Int32 => "Int32".into(),
-            BytecodeType::Int64 => "Int64".into(),
-            BytecodeType::Float32 => "Float32".into(),
-            BytecodeType::Float64 => "Float64".into(),
-            BytecodeType::Bool => "Bool".into(),
-            BytecodeType::Ptr => panic!("type Ptr only for internal use."),
+            BytecodeType::Unit => write!(fmt, "()"),
+            BytecodeType::UInt8 => write!(fmt, "UInt8"),
+            BytecodeType::Char => write!(fmt, "Char"),
+            BytecodeType::Int32 => write!(fmt, "Int32"),
+            BytecodeType::Int64 => write!(fmt, "Int64"),
+            BytecodeType::Float32 => write!(fmt, "Float32"),
+            BytecodeType::Float64 => write!(fmt, "Float64"),
+            BytecodeType::Bool => write!(fmt, "Bool"),
+            BytecodeType::Ptr => write!(fmt, "Ptr"),
             BytecodeType::Class(id, type_params) => {
-                let cls = self.vm.classes.idx(id);
+                let cls = self.vm.classes.idx(*id);
                 let cls = cls.read();
                 let name = self.vm.interner.str(cls.name).to_string();
-                self.with_type_params(name, &type_params)
+                write!(fmt, "{}", name)?;
+                self.type_params(type_params, fmt)
             }
             BytecodeType::Struct(sid, type_params) => {
-                let struc = self.vm.structs.idx(sid);
+                let struc = self.vm.structs.idx(*sid);
                 let struc = struc.read();
                 let name = struc.name;
                 let name = self.vm.interner.str(name).to_string();
-                self.with_type_params(name, &type_params)
+                write!(fmt, "{}", name)?;
+                self.type_params(type_params, fmt)
             }
             BytecodeType::Trait(tid, type_params) => {
-                let trait_ = self.vm.traits[tid].read();
+                let trait_ = self.vm.traits.idx(*tid);
+                let trait_ = trait_.read();
                 let name = self.vm.interner.str(trait_.name).to_string();
-                self.with_type_params(name, &type_params)
+                write!(fmt, "{}", name)?;
+                self.type_params(type_params, fmt)
             }
             BytecodeType::Enum(id, type_params) => {
-                let enum_ = self.vm.enums[id].read();
+                let enum_ = self.vm.enums.idx(*id);
+                let enum_ = enum_.read();
                 let name = self.vm.interner.str(enum_.name).to_string();
-                self.with_type_params(name, &type_params)
+                write!(fmt, "{}", name)?;
+                self.type_params(type_params, fmt)
             }
 
             BytecodeType::TypeParam(idx) => {
                 if let Some(type_params) = self.type_params {
-                    self.vm
+                    let name = self
+                        .vm
                         .interner
-                        .str(type_params.name(TypeParamId(idx as usize)))
-                        .to_string()
+                        .str(type_params.name(TypeParamId(*idx as usize)))
+                        .to_string();
+                    write!(fmt, "{}", name)
                 } else {
-                    format!("TypeParam({})", idx)
+                    write!(fmt, "TypeParam({})", idx)
                 }
             }
 
             BytecodeType::Lambda(params, return_type) => {
-                let params = self.type_list(&params);
-                let ret = self.name(*return_type);
-                format!("({}) -> {}", params, ret)
+                write!(fmt, "(")?;
+                self.type_list(params, fmt)?;
+                write!(fmt, ") -> ")?;
+                self.name(return_type, fmt)
             }
 
             BytecodeType::Tuple(subtypes) => {
-                let types = self.type_list(&subtypes);
-                format!("({})", types)
+                write!(fmt, "(")?;
+                self.type_list(subtypes, fmt)?;
+                write!(fmt, ") -> ")
             }
         }
     }
 
-    fn with_type_params(&self, name: String, types: &BytecodeTypeArray) -> String {
+    fn type_params(
+        &self,
+        types: &BytecodeTypeArray,
+        fmt: &mut std::fmt::Formatter,
+    ) -> std::fmt::Result {
         if types.is_empty() {
-            return name;
+            return Ok(());
         }
 
-        let params = types
-            .iter()
-            .map(|ty| self.name(ty))
-            .collect::<Vec<_>>()
-            .join(", ");
-
-        format!("{}[{}]", &name, params)
+        write!(fmt, "[")?;
+        self.type_list(types, fmt)?;
+        write!(fmt, "]")
     }
 
-    fn type_list(&self, types: &BytecodeTypeArray) -> String {
-        types
-            .iter()
-            .map(|ty| self.name(ty))
-            .collect::<Vec<_>>()
-            .join(", ")
+    fn type_list(
+        &self,
+        types: &BytecodeTypeArray,
+        fmt: &mut std::fmt::Formatter,
+    ) -> std::fmt::Result {
+        let mut first = true;
+        for ty in types.iter() {
+            if first {
+                write!(fmt, ", ")?;
+            }
+            self.name(&ty, fmt)?;
+            first = false;
+        }
+
+        Ok(())
     }
 }
 
 impl<'a> std::fmt::Display for BytecodeTypePrinter<'a> {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "{}", self.string())
+        self.name(&self.ty, f)
     }
 }
 
