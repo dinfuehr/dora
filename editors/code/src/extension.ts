dbcd3fab6fd9e14587302a9df1b4bf93bef0575a
Dominik Inführ <dominik.infuehr@gmail.com>
2023-04-08 12:40:01 +0200

    code: experiment with status bar item

--- editors/code/src/extension.ts
+++ editors/code/src/extension.ts
@@ -3,6 +3,10 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
 	console.log('activate extension');
 
+	const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 0);
+	statusBarItem.text = "dora-server";
+	statusBarItem.show();
+
 	let disposable = vscode.commands.registerCommand('dora-lang.ping', () => {
 		vscode.window.showInformationMessage("Action from Dora extension.");
 	});
