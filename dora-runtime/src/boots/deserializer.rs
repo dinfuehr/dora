9da2a932aac3561508b141e5fc8e5535abb69c15
Dominik Inführ <dominik.infuehr@gmail.com>
2023-09-07 21:32:20 +0200

    boots: add more serializer/deserializer for CodeDescriptor

--- dora-runtime/src/boots/deserializer.rs
+++ dora-runtime/src/boots/deserializer.rs
@@ -1,21 +1,40 @@
 use crate::constpool::ConstPool;
+use crate::gc::Address;
 use crate::masm::CodeDescriptor;
-use crate::vm::{CommentTable, GcPointTable, LazyCompilationData, LocationTable, RelocationTable};
+use crate::vm::{
+    CommentTable, GcPoint, GcPointTable, LazyCompilationData, LocationTable, RelocationTable,
+    CODE_ALIGNMENT,
+};
 use dora_bytecode::Location;
 
 pub fn decode_code_descriptor(reader: &mut ByteReader) -> CodeDescriptor {
+    let constpool = decode_const_pool(reader);
     let code = decode_code(reader);
-    let comments = decode_comment_table(reader);
+    let lazy_compilation = decode_lazy_compilation(reader);
+    let gcpoints = decode_gcpoint_table(reader);
     let positions = decode_location_table(reader);
+    let comments = decode_comment_table(reader);
     CodeDescriptor {
         code,
         comments,
-        constpool: ConstPool::new(),
-        lazy_compilation: LazyCompilationData::new(),
-        gcpoints: GcPointTable::new(),
+        constpool,
+        lazy_compilation,
+        gcpoints,
         positions,
         relocations: RelocationTable::new(),
     }
+}
+
+fn decode_const_pool(reader: &mut ByteReader) -> ConstPool {
+    let length = reader.read_u32() as usize;
+    let mut pool = ConstPool::new();
+
+    for _ in 0..length {
+        pool.add_addr(reader.read_address());
+    }
+
+    pool.align(CODE_ALIGNMENT as i32);
+    pool
 }
 
 fn decode_code(reader: &mut ByteReader) -> Vec<u8> {
@@ -60,6 +79,36 @@ fn decode_comment_table(reader: &mut ByteReader) -> CommentTable {
     result
 }
 
+fn decode_lazy_compilation(reader: &mut ByteReader) -> LazyCompilationData {
+    let length = reader.read_u32() as usize;
+    assert_eq!(length, 0);
+    LazyCompilationData::new()
+}
+
+fn decode_gcpoint_table(reader: &mut ByteReader) -> GcPointTable {
+    let length = reader.read_u32() as usize;
+    let mut result = GcPointTable::new();
+
+    for _ in 0..length {
+        let offset = reader.read_u32();
+        let gcpoint = decode_gcpoint(reader);
+        result.insert(offset, gcpoint);
+    }
+
+    result
+}
+
+fn decode_gcpoint(reader: &mut ByteReader) -> GcPoint {
+    let length = reader.read_u32() as usize;
+    let mut offsets = Vec::with_capacity(length);
+
+    for _ in 0..length {
+        offsets.push(reader.read_u32() as i32);
+    }
+
+    GcPoint::from_offsets(offsets)
+}
+
 fn decode_string(reader: &mut ByteReader) -> String {
     let array = decode_array_u8(reader);
     String::from_utf8(array).expect("invalid encoding")
@@ -88,6 +137,17 @@ impl ByteReader {
         let b4 = self.read_u8() as u32;
 
         (b4 << 24) | (b3 << 16) | (b2 << 8) | b1
+    }
+
+    pub fn read_u64(&mut self) -> u64 {
+        let w1 = self.read_u32() as u64;
+        let w2 = self.read_u32() as u64;
+
+        (w2 << 32) | w1
+    }
+
+    pub fn read_address(&mut self) -> Address {
+        (self.read_u64() as usize).into()
     }
 
     pub fn has_more(&self) -> bool {
