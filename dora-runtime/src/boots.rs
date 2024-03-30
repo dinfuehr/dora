47091ca9aa1e5ad297298c5044d695d2b6b09224
Dominik Inführ <dominik.infuehr@gmail.com>
2024-03-30 08:33:00 +0100

    boots: Use computeTupleLayout for tuple element loads

--- dora-runtime/src/boots.rs
+++ dora-runtime/src/boots.rs
@@ -2,7 +2,7 @@ use std::mem;
 use std::ptr;
 
 use dora_bytecode::{
-    BytecodeType, BytecodeTypeArray, ClassId, FunctionId, FunctionKind, GlobalId, StructId, TraitId,
+    BytecodeType, BytecodeTypeArray, ClassId, FunctionId, FunctionKind, GlobalId, TraitId,
 };
 
 use crate::boots::deserializer::{decode_code_descriptor, ByteReader};
@@ -16,10 +16,7 @@ use crate::object::Str;
 use crate::object::{Ref, UInt8Array};
 use crate::size::InstanceSize;
 use crate::threads::current_thread;
-use crate::vm::{
-    create_class_instance, create_struct_instance, get_concrete_tuple_bty, get_vm, impls,
-    CodeDescriptor, VM,
-};
+use crate::vm::{create_class_instance, get_vm, impls, CodeDescriptor, VM};
 
 use self::deserializer::{decode_bytecode_type, decode_bytecode_type_array};
 use self::serializer::allocate_encoded_struct_data;
@@ -93,14 +90,6 @@ pub const BOOTS_NATIVE_FUNCTIONS: &[(&'static str, *const u8)] = &[
     (
         "boots::interface::getIntrinsicForFunctionRaw",
         get_intrinsic_for_function_raw as *const u8,
-    ),
-    (
-        "boots::interface::getStructFieldOffsetRaw",
-        get_struct_field_offset_raw as *const u8,
-    ),
-    (
-        "boots::interface::getTupleElementOffsetRaw",
-        get_tuple_element_offset_raw as *const u8,
     ),
     (
         "boots::interface::getStructDataRaw",
@@ -429,52 +418,6 @@ extern "C" fn get_intrinsic_for_function_raw(id: u32) -> i32 {
     let vm = get_vm();
     let fct = &vm.program.functions[id as usize];
     fct.intrinsic.map(|i| i as u32 as i32).unwrap_or(-1)
-}
-
-extern "C" fn get_struct_field_offset_raw(data: Handle<UInt8Array>) -> i32 {
-    let vm = get_vm();
-
-    let mut serialized_data = vec![0; data.len()];
-
-    unsafe {
-        ptr::copy_nonoverlapping(
-            data.data() as *mut u8,
-            serialized_data.as_mut_ptr(),
-            data.len(),
-        );
-    }
-
-    let mut reader = ByteReader::new(serialized_data);
-    let struct_id = StructId(reader.read_u32());
-    let type_params = decode_bytecode_type_array(&mut reader);
-    let field_id = reader.read_u32();
-    assert!(!reader.has_more());
-
-    let struct_instance_id = create_struct_instance(vm, struct_id, type_params);
-    let struct_instance = vm.struct_instances.idx(struct_instance_id);
-    struct_instance.fields[field_id as usize].offset
-}
-
-extern "C" fn get_tuple_element_offset_raw(data: Handle<UInt8Array>) -> i32 {
-    let vm = get_vm();
-
-    let mut serialized_data = vec![0; data.len()];
-
-    unsafe {
-        ptr::copy_nonoverlapping(
-            data.data() as *mut u8,
-            serialized_data.as_mut_ptr(),
-            data.len(),
-        );
-    }
-
-    let mut reader = ByteReader::new(serialized_data);
-    let tuple_ty = decode_bytecode_type(&mut reader);
-    let subtype_idx = reader.read_u32();
-    assert!(!reader.has_more());
-
-    let tuple = get_concrete_tuple_bty(vm, &tuple_ty);
-    tuple.offsets()[subtype_idx as usize]
 }
 
 extern "C" fn get_struct_data_raw(id: u32) -> Ref<UInt8Array> {
