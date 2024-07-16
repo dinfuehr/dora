07f93fdfb1f45e593477b36630ca865a63f662f1
Dominik Inführ <dominik.infuehr@gmail.com>
2024-07-16 22:19:59 +0200

    Switch to new testing mode for boots

--- tools/test-release.bat
+++ tools/test-release.bat
@@ -1,1 +1,1 @@
-cargo build --release && cargo test --release && ruby tools\tester.rb --release %* && cargo run -p dora --release -- test dora-boots/boots.dora --gc-verify
+cargo build --release && cargo test --release && ruby tools\tester.rb --release %* && cargo run -p dora --release -- test --package boots dora-boots/boots.dora --test-boots tests/hello-world.dora --gc-verify
--- tools/test.bat
+++ tools/test.bat
@@ -1,1 +1,1 @@
-cargo build && cargo test && ruby tools\tester.rb %* && cargo run -p dora -- test dora-boots/boots.dora --gc-verify
+cargo build && cargo test && ruby tools\tester.rb %* && cargo run -p dora -- test --package boots dora-boots/boots.dora --test-boots tests/hello-world.dora --gc-verify
