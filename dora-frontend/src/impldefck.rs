a30ff383a87c20f56405f2f5c0c5e84619636e62
Dominik Inführ <dominik.infuehr@gmail.com>
2024-10-04 19:31:50 +0200

    frontend: Add interesting/unsupported test case

--- dora-frontend/src/impldefck.rs
+++ dora-frontend/src/impldefck.rs
@@ -1020,11 +1020,23 @@ mod tests {
 
     #[test]
     #[ignore]
-    fn generic_impl() {
+    fn impl_of_generic_trait() {
         ok("
             trait Foo[T] {}
             impl[T] Foo[T] for Int64 {}
             fn f(x: Int64): Foo[String] { x as Foo[String] }
+        ");
+    }
+
+    #[test]
+    #[ignore]
+    fn impl_generic_extended_ty() {
+        ok("
+            trait Foo[T] { fn get(): T; }
+            impl[T] Foo[T] for T {
+                fn get(): T { self }
+            }
+            fn f(x: Int64): Foo[Int64] { x as Foo[Int64] }
         ");
     }
 }
