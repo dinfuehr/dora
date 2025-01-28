84315c331f1d83bfa3f41a78758e922871a522fc
Dominik Inführ <dominik.infuehr@gmail.com>
2025-01-28 22:32:00 +0100

    tools: Make use of local variables in script

--- tools/build.ps1
+++ tools/build.ps1
@@ -5,15 +5,19 @@ $build_profile = "release"
 cargo build -p dora --profile $build_profile
 cargo build -p dora-language-server --profile $build_profile
 
-# Remove old install
-Remove-Item -Path "$Home\.dora" -Recurse -Force
+$install_dir = "$Home\.dora"
+$install_dir_bin = "$install_dir\bin"
+$install_dir_pkgs = "$install_dir\pkgs"
 
-$null = New-Item -Path "$Home\.dora" -ItemType "directory"
-$null = New-Item -Path "$Home\.dora\bin" -ItemType "directory"
-$null = New-Item -Path "$Home\.dora\pkgs" -ItemType "directory"
+# Remove old install
+Remove-Item -Path $install_dir -Recurse -Force
+
+$null = New-Item -Path $install_dir -ItemType "directory"
+$null = New-Item -Path $install_dir_bin -ItemType "directory"
+$null = New-Item -Path $install_dir_pkgs -ItemType "directory"
 
 Copy-Item -Path "target/$build_profile/dora.exe" -Destination "$Home\.dora\bin"
 Copy-Item -Path "target/$build_profile/dora-language-server.exe" -Destination "$Home\.dora\bin"
 
-Copy-Item -Path "pkgs\std" -Destination "$Home\.dora\pkgs" -Recurse
-Copy-Item -Path "pkgs\boots" -Destination "$Home\.dora\pkgs" -Recurse
+Copy-Item -Path "pkgs\std" -Destination $install_dir_pkgs -Recurse
+Copy-Item -Path "pkgs\boots" -Destination $install_dir_pkgs -Recurse
