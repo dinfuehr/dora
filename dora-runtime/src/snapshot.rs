f27f68a8ce9dd1e4360b882efe245b71f4e1e2b3
Dominik Inführ <dominik.infuehr@gmail.com>
2025-05-29 07:54:59 +0200

    rt: Add nodes_map for heap snapshots

--- dora-runtime/src/snapshot.rs
+++ dora-runtime/src/snapshot.rs
@@ -2,14 +2,17 @@ use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufWriter, Result as IoResult, Write};
 
+use crate::gc::Address;
 use crate::vm::VM;
 
 struct SnapshotGenerator<'a> {
     writer: &'a dyn Write,
     nodes: Vec<Node>,
+    nodes_map: HashMap<Address, NodeId>,
     edges: Vec<Edge>,
     strings: Vec<String>,
     strings_map: HashMap<String, usize>,
+    meta_space_start: Address,
 }
 
 impl<'a> SnapshotGenerator<'a> {
@@ -20,9 +23,11 @@ impl<'a> SnapshotGenerator<'a> {
         let mut generator = SnapshotGenerator {
             writer: &mut writer,
             nodes: Vec::new(),
+            nodes_map: HashMap::new(),
             edges: Vec::new(),
             strings: Vec::new(),
             strings_map: HashMap::new(),
+            meta_space_start: vm.meta_space_start(),
         };
         generator.iterate_heap(vm);
         generator.dump()?;
@@ -30,10 +35,26 @@ impl<'a> SnapshotGenerator<'a> {
     }
 
     fn iterate_heap(&mut self, vm: &VM) {
-        vm.gc
-            .collector()
-            .to_swiper()
-            .iterate_heap(vm, |_address| unimplemented!());
+        let swiper = vm.gc.collector().to_swiper();
+        swiper.iterate_heap(vm, |address| {
+            let node_id = self.ensure_node(address);
+
+            let object = address.to_obj();
+            let size = object.size(self.meta_space_start);
+            self.node_mut(node_id).self_size = size;
+        });
+    }
+
+    fn ensure_node(&mut self, address: Address) -> NodeId {
+        *self.nodes_map.entry(address).or_insert_with(|| {
+            let id = NodeId(self.nodes.len());
+            self.nodes.push(Default::default());
+            id
+        })
+    }
+
+    fn node_mut(&mut self, id: NodeId) -> &mut Node {
+        &mut self.nodes[id.0]
     }
 
     fn dump(&self) -> IoResult<()> {
@@ -41,6 +62,10 @@ impl<'a> SnapshotGenerator<'a> {
     }
 }
 
+#[derive(Clone, Copy)]
+struct NodeId(usize);
+
+#[derive(Default)]
 struct Node {
     edge_count: usize,
     self_size: usize,
@@ -48,5 +73,5 @@ struct Node {
 
 struct Edge {
     name_or_idx: usize,
-    to_nodex_index: usize,
+    to_node_index: NodeId,
 }
