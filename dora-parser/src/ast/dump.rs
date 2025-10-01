c6393adb385b0a16cb87edaf45090d43660c74e1
Dominik Inführ <dominik.infuehr@gmail.com>
2025-10-01 20:10:19 +0200

    parser: Improve Ast dump

--- dora-parser/src/ast/dump.rs
+++ dora-parser/src/ast/dump.rs
@@ -1,6 +1,7 @@
 use std::sync::Arc;
 
 use crate::ast::*;
+use crate::{compute_line_column, compute_line_starts, Span};
 
 macro_rules! dump {
     ($self_:ident, $($message:tt)*) => {{
@@ -13,21 +14,29 @@ macro_rules! dump {
 }
 
 pub fn dump_file(ast: &Arc<File>) {
+    let line_starts = compute_line_starts(&ast.content);
     let mut dumper = AstDumper {
         indent: 0,
         f: ast.as_ref(),
+        line_starts,
     };
     dumper.dump_file();
 }
 
 pub fn dump_node(f: &Arc<File>, id: AstId) {
-    let mut dumper = AstDumper { indent: 0, f };
+    let line_starts = compute_line_starts(&f.content);
+    let mut dumper = AstDumper {
+        indent: 0,
+        f,
+        line_starts,
+    };
     dumper.dump_node_id(id);
 }
 
 struct AstDumper<'a> {
     indent: u32,
     f: &'a File,
+    line_starts: Vec<u32>,
 }
 
 impl<'a> AstDumper<'a> {
@@ -41,8 +50,37 @@ impl<'a> AstDumper<'a> {
         self.dump_node(id, self.f.node(id));
     }
 
+    fn format_span(&self, span: Span) -> String {
+        let (start_line, start_col) = compute_line_column(&self.line_starts, span.start());
+        let (end_line, end_col) = compute_line_column(&self.line_starts, span.end());
+        format!("L{}:{}-L{}:{}", start_line, start_col, end_line, end_col)
+    }
+
+    fn node_extra_info(&self, el: &Ast) -> Option<String> {
+        match el {
+            Ast::LitChar(lit) => Some(lit.value.clone()),
+            Ast::LitInt(lit) => Some(lit.value.clone()),
+            Ast::LitFloat(lit) => Some(lit.value.clone()),
+            Ast::LitStr(lit) => Some(format!("{:?}", lit.value)),
+            Ast::LitBool(lit) => Some(lit.value.to_string()),
+            Ast::Ident(ident) => Some(ident.name.clone()),
+            Ast::Bin(bin) => Some(bin.op.as_str().to_string()),
+            Ast::Un(un) => Some(un.op.as_str().to_string()),
+            _ => None,
+        }
+    }
+
     fn dump_node(&mut self, id: AstId, el: &Ast) {
-        dump!(self, "{} {} #{}", el.name(), el.span(), id.index());
+        let name = el.name();
+        let span = self.format_span(el.span());
+        let id_str = id.index();
+
+        if let Some(extra) = self.node_extra_info(el) {
+            dump!(self, "{} {} #{} {}", name, extra, id_str, span);
+        } else {
+            dump!(self, "{} #{} {}", name, id_str, span);
+        }
+
         let children = el.children();
         if !children.is_empty() {
             self.indent(|d| {
