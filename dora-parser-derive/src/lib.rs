419766a3237ac7488992ddae363edc482d08997c
Dominik Inführ <dominik.infuehr@gmail.com>
2025-10-02 21:05:44 +0200

    parser: Add AstEnum proc macro

--- dora-parser-derive/src/lib.rs
+++ dora-parser-derive/src/lib.rs
@@ -1,6 +1,8 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{Data, DeriveInput, Fields, GenericArgument, PathArguments, Type, parse_macro_input};
+use syn::{
+    Data, DataEnum, DeriveInput, Fields, GenericArgument, PathArguments, Type, parse_macro_input,
+};
 
 #[proc_macro_derive(AstNode)]
 pub fn derive_ast_node(input: TokenStream) -> TokenStream {
@@ -133,4 +135,173 @@ fn is_option_vec_ast_id(ty: &Type) -> bool {
         }
     }
     false
+}
+
+#[proc_macro_derive(AstEnum)]
+pub fn derive_ast_enum(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let enum_name = &input.ident;
+
+    let data_enum = match &input.data {
+        Data::Enum(data) => data,
+        _ => panic!("AstEnum can only be derived for enums"),
+    };
+
+    let variant_methods = generate_variant_methods(data_enum);
+    let span_method = generate_span_method(data_enum);
+    let name_method = generate_name_method(data_enum);
+    let children_method = generate_children_method(data_enum);
+
+    let expanded = quote! {
+        impl #enum_name {
+            #span_method
+            #name_method
+            #children_method
+            #variant_methods
+        }
+    };
+
+    TokenStream::from(expanded)
+}
+
+fn generate_variant_methods(data_enum: &DataEnum) -> proc_macro2::TokenStream {
+    let mut methods = Vec::new();
+
+    for variant in &data_enum.variants {
+        let variant_name = &variant.ident;
+
+        // Get the inner type if it's a tuple variant with one field
+        let inner_type = match &variant.fields {
+            Fields::Unnamed(fields) if fields.unnamed.len() == 1 => {
+                Some(&fields.unnamed.first().unwrap().ty)
+            }
+            _ => None,
+        };
+
+        if let Some(inner_ty) = inner_type {
+            // Convert variant name to snake_case for method names
+            let method_name_str = to_snake_case(&variant_name.to_string());
+            let is_method_name =
+                syn::Ident::new(&format!("is_{}", method_name_str), variant_name.span());
+            let to_method_name =
+                syn::Ident::new(&format!("to_{}", method_name_str), variant_name.span());
+
+            // Generate is_<variant> method
+            let is_method = quote! {
+                pub fn #is_method_name(&self) -> bool {
+                    match self {
+                        Self::#variant_name(..) => true,
+                        _ => false,
+                    }
+                }
+            };
+
+            // Generate to_<variant> method
+            let to_method = quote! {
+                pub fn #to_method_name(&self) -> Option<&#inner_ty> {
+                    match self {
+                        Self::#variant_name(inner) => Some(inner),
+                        _ => None,
+                    }
+                }
+            };
+
+            methods.push(is_method);
+            methods.push(to_method);
+        }
+    }
+
+    quote! {
+        #(#methods)*
+    }
+}
+
+fn generate_span_method(data_enum: &DataEnum) -> proc_macro2::TokenStream {
+    let mut match_arms = Vec::new();
+
+    for variant in &data_enum.variants {
+        let variant_name = &variant.ident;
+
+        // Only handle tuple variants with one field
+        if matches!(&variant.fields, Fields::Unnamed(fields) if fields.unnamed.len() == 1) {
+            let arm = quote! {
+                Self::#variant_name(node) => node.span
+            };
+            match_arms.push(arm);
+        }
+    }
+
+    quote! {
+        pub fn span(&self) -> Span {
+            match self {
+                #(#match_arms),*
+            }
+        }
+    }
+}
+
+fn generate_name_method(data_enum: &DataEnum) -> proc_macro2::TokenStream {
+    let mut match_arms = Vec::new();
+
+    for variant in &data_enum.variants {
+        let variant_name = &variant.ident;
+
+        // Only handle tuple variants with one field
+        if matches!(&variant.fields, Fields::Unnamed(fields) if fields.unnamed.len() == 1) {
+            let arm = quote! {
+                Self::#variant_name(node) => node.name()
+            };
+            match_arms.push(arm);
+        }
+    }
+
+    quote! {
+        pub fn name(&self) -> &'static str {
+            match self {
+                #(#match_arms),*
+            }
+        }
+    }
+}
+
+fn generate_children_method(data_enum: &DataEnum) -> proc_macro2::TokenStream {
+    let mut match_arms = Vec::new();
+
+    for variant in &data_enum.variants {
+        let variant_name = &variant.ident;
+
+        // Only handle tuple variants with one field
+        if matches!(&variant.fields, Fields::Unnamed(fields) if fields.unnamed.len() == 1) {
+            let arm = quote! {
+                Self::#variant_name(node) => node.children()
+            };
+            match_arms.push(arm);
+        }
+    }
+
+    quote! {
+        pub fn children(&self) -> Vec<AstId> {
+            match self {
+                #(#match_arms),*
+            }
+        }
+    }
+}
+
+fn to_snake_case(s: &str) -> String {
+    let mut result = String::new();
+    let mut chars = s.chars().peekable();
+
+    while let Some(ch) = chars.next() {
+        if ch.is_uppercase() {
+            if !result.is_empty() {
+                result.push('_');
+            }
+            result.push(ch.to_lowercase().next().unwrap());
+        } else {
+            result.push(ch);
+        }
+    }
+
+    result
 }
