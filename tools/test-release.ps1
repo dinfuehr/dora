7373b3ecef18fa50c30773b2393e3d0e7fa60ac6
Dominik Inführ <dominik.infuehr@gmail.com>
2025-11-15 10:24:05 +0100

    Switch to pytester

--- tools/test-release.ps1
+++ tools/test-release.ps1
@@ -15,7 +15,7 @@ Check-Exit-Code
 cargo run -p dora --release -- --boots --report-all-warnings --emit-compiler --bootstrap-compiler tests/boots/hello.dora
 Check-Exit-Code
 
-ruby tools/tester.rb --release $args
+.\tools\rt.ps1 --release $args
 Check-Exit-Code
 
 cargo run -p dora --release -- test --boots --test-boots --gc-verify tests/hello-world.dora
--- tools/test.ps1
+++ tools/test.ps1
@@ -15,7 +15,7 @@ Check-Exit-Code
 cargo run -p dora -- --boots --report-all-warnings --emit-compiler --bootstrap-compiler tests/boots/hello.dora
 Check-Exit-Code
 
-ruby tools/tester.rb $args
+.\tools\rt.ps1 $args
 Check-Exit-Code
 
 cargo run -p dora -- test --boots --test-boots --gc-verify tests/hello-world.dora
