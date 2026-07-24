b049c0cfb345baeaeee96a36f226c586c87fde4b
Dominik Inführ <dominik.infuehr@gmail.com>
2026-07-24 09:41:37 +0200

    Pass expected type through paren expressions

--- dora-frontend/src/typeck/expr/paren.rs
+++ dora-frontend/src/typeck/expr/paren.rs
@@ -6,9 +6,9 @@ pub(super) fn check_expr_paren(
     ck: &mut TypeCheck,
     expr_id: ExprId,
     subexpr_id: ExprId,
-    _expected_ty: SourceType,
+    expected_ty: SourceType,
 ) -> SourceType {
-    let ty = check_expr(ck, subexpr_id, SourceType::Any);
+    let ty = check_expr(ck, subexpr_id, expected_ty);
     ck.body.set_ty(expr_id, ty.clone());
 
     ty
