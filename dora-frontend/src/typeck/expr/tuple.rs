2b6993b7850927c0c8e56dd4a3a9452fb8e1e6f0
Dominik Inführ <dominik.infuehr@gmail.com>
2026-07-24 09:58:04 +0200

    Pass down expected type to tuple arguments

--- dora-frontend/src/typeck/expr/tuple.rs
+++ dora-frontend/src/typeck/expr/tuple.rs
@@ -7,17 +7,22 @@ pub(super) fn check_expr_tuple(
     ck: &mut TypeCheck,
     expr_id: ExprId,
     sema_expr: &TupleExpr,
-    _expected_ty: SourceType,
+    expected_ty: SourceType,
 ) -> SourceType {
     if sema_expr.values.is_empty() {
         ck.body.set_ty(expr_id, SourceType::Unit);
         return SourceType::Unit;
     }
 
+    let expected_subtypes = expected_ty.tuple_subtypes();
     let mut subtypes = Vec::new();
 
-    for &value_id in &sema_expr.values {
-        let subtype = check_expr(ck, value_id, SourceType::Any);
+    for (idx, &value_id) in sema_expr.values.iter().enumerate() {
+        let expected_subtype = expected_subtypes
+            .as_ref()
+            .and_then(|subtypes| subtypes.types().get(idx).cloned())
+            .unwrap_or(SourceType::Any);
+        let subtype = check_expr(ck, value_id, expected_subtype);
         subtypes.push(subtype);
     }
 
